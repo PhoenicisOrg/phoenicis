b2831509b8c54a240333eb5738601e3a4067ed66
Quentin PÂRIS <git@qparis.fr>
2015-04-13 19:08:55 +0100

    Removing target folder

---
+++ src/main/python/v4wrapper.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# This tools is made to run legacy PlayOnLinux v4 scripts
+
+if __name__ == '__main__':
+    print "Ok"
---
+++ src/test/java/scripts/ScriptTest.java
@@ -0,0 +1,22 @@
+package scripts;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tinou on 12/04/15.
+ */
+public class ScriptTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDetectScriptType() throws Exception {
+
+    }
+}
