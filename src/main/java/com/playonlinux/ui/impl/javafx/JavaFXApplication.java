9b8bc7c168aeb75020ab502349d5b9e1a39d3c13
Quentin PÂRIS <git@qparis.fr>
2015-05-05 06:58:48 +0100

    Code cleaning

--- src/main/java/com/playonlinux/ui/impl/javafx/JavaFXApplication.java
+++ src/main/java/com/playonlinux/ui/impl/javafx/JavaFXApplication.java
@@ -9,13 +9,14 @@ import com.playonlinux.ui.impl.javafx.mainwindow.MainWindow;
 public class JavaFXApplication extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         MainWindow mainWindow = new MainWindow();
 
         mainWindow.setUpWindow();
         try {
             mainWindow.setUpEvents();
         } catch (PlayOnLinuxError e) {
+            // FIXME
             e.printStackTrace();
         }
         mainWindow.show();
