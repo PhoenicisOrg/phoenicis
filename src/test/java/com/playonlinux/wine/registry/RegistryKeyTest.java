1a385fa0dbf59c14a2e918ae1c2165ac5592d08f
Quentin PÂRIS <git@qparis.fr>
2015-05-11 00:15:31 +0100

    [Registry] test ToString method

--- src/test/java/com/playonlinux/wine/registry/RegistryKeyTest.java
+++ src/test/java/com/playonlinux/wine/registry/RegistryKeyTest.java
@@ -18,6 +18,7 @@
 
 package com.playonlinux.wine.registry;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -25,13 +26,20 @@ import static org.junit.Assert.*;
 
 public class RegistryKeyTest {
 
-    @Test
-    public void testRegistryKey_testGetChildWithMultipleArguments() throws Exception {
-        RegistryKey a1 = new RegistryKey("A1");
-        RegistryKey b1 = new RegistryKey("B1");
-        RegistryKey b2 = new RegistryKey("B2");
-        RegistryKey c1 = new RegistryKey("C1");
-        RegistryKey c2 = new RegistryKey("C2");
+    RegistryKey a1;
+    RegistryKey b1;
+    RegistryKey b2;
+    RegistryKey c1;
+    RegistryKey c2;
+
+
+    @Before
+    public void setUp() {
+        a1 = new RegistryKey("A1");
+        b1 = new RegistryKey("B1");
+        b2 = new RegistryKey("B2");
+        c1 = new RegistryKey("C1");
+        c2 = new RegistryKey("C2");
 
         RegistryValue<StringValueType> v1 = new RegistryValue<>("V1", new StringValueType("Content 1"));
         RegistryValue<StringValueType> v2 = new RegistryValue<>("V2", new StringValueType("Content 2"));
@@ -45,11 +53,27 @@ public class RegistryKeyTest {
         c1.addChild(v1);
         c2.addChild(v2);
         c2.addChild(v3);
+    }
 
+
+    @Test
+    public void testRegistryKey_testGetChildWithMultipleArguments() throws Exception {
         assertEquals("Content 1", ((RegistryValue) a1.getChild("B2", "C1", "V1")).getText());
         assertEquals("Content 2", ((RegistryValue) a1.getChild("B2", "C2", "V2")).getText() );
         assertEquals("Content 3", ((RegistryValue) a1.getChild("B2", "C2", "V3")).getText() );
 
         assertEquals("C1", a1.getChild("B2", "C1").getName());
+    }
+
+    @Test
+    public void testRegistryKey_toString_goodFormat() {
+        assertEquals("+ A1\n" +
+                "| + B1\n" +
+                "| + B2\n" +
+                "| | + C1\n" +
+                "| | | - (StringValueType) V1 -> Content 1\n" +
+                "| | + C2\n" +
+                "| | | - (StringValueType) V2 -> Content 2\n" +
+                "| | | - (StringValueType) V3 -> Content 3\n", a1.toString());
     }
 }
