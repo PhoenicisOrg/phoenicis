215a99a0d37494c3a28cc0681e77d6f807ccb042
Markus Ebner <hiwatari.seiji@gmail.com>
2015-05-19 23:46:09 +0200

    Add POL_Download to legacyScriptExample.

--- samples/legacyScriptExemple.sh
+++ samples/legacyScriptExemple.sh
@@ -6,9 +6,9 @@ TITLE="Legacy script"
 
 echo "Test"
 POL_SetupWindow_Init
+POL_Download "http://files.metaquotes.net/metaquotes.software.corp/mt4/mt4setup.exe" "e42ebc9ebc2d8975de5a0c94386d1a6a"
 POL_SetupWindow_message "Test"
 POL_SetupWindow_message "Test 2"
 POL_SetupWindow_message "Test 3"
-
 POL_SetupWindow_Close
-exit 
+exit
