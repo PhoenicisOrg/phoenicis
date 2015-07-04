7687fe0cdc38a403789eb839188321713fab6214
Quentin PÂRIS <git@qparis.fr>
2015-07-04 23:17:30 +0100

    [Console] Used background service

--- src/main/java/com/playonlinux/domain/RemoteAvailableInstallersPlayOnLinuxImplementation.java
+++ src/main/java/com/playonlinux/domain/RemoteAvailableInstallersPlayOnLinuxImplementation.java
@@ -110,7 +110,7 @@ public final class RemoteAvailableInstallersPlayOnLinuxImplementation extends Ob
     }
 
     @Override
-    public ConList<AppsItemDTO> getFiltered(Filter<AppsItemDTO> filter) {
+    public List<AppsItemDTO> getFiltered(Filter<AppsItemDTO> filter) {
         List<AppsItemDTO> filtered = new ArrayList<>();
         for(AppsItemDTO item : this){
             if(filter.apply(item)){
