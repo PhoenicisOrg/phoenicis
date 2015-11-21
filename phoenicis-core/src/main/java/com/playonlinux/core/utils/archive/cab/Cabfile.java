97bc8f6d3a5be9fd8e6e8debe97dc80bcc926ffc
Kévin Rauscher <kevin.rauscher@tomahna.fr>
2015-11-21 17:07:19 +0100

    removing unused CompressionType

--- phoenicis-core/src/main/java/com/playonlinux/core/utils/archive/cab/Cabfile.java
+++ phoenicis-core/src/main/java/com/playonlinux/core/utils/archive/cab/Cabfile.java
@@ -46,25 +46,25 @@ public class Cabfile {
                 byte[] nextRead = new byte[1];
                 int numRead = archiveStream.read(nextRead);
 
-                if(numRead == -1) {
+                if (numRead == -1) {
                     throw new CabException("This file does not contain any cabinet archive");
                 }
 
-                if((nextRead[0] & 0xFF) == 0x4D && successBytes == 0) {
+                if ((nextRead[0] & 0xFF) == 0x4D && successBytes == 0) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x53 && successBytes == 1) {
+                } else if ((nextRead[0] & 0xFF) == 0x53 && successBytes == 1) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x43 && successBytes == 2) {
+                } else if ((nextRead[0] & 0xFF) == 0x43 && successBytes == 2) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x46 && successBytes == 3) {
+                } else if ((nextRead[0] & 0xFF) == 0x46 && successBytes == 3) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x00 && successBytes == 4) {
+                } else if ((nextRead[0] & 0xFF) == 0x00 && successBytes == 4) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x00 && successBytes == 5) {
+                } else if ((nextRead[0] & 0xFF) == 0x00 && successBytes == 5) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x00 && successBytes == 6) {
+                } else if ((nextRead[0] & 0xFF) == 0x00 && successBytes == 6) {
                     successBytes++;
-                } else if((nextRead[0] & 0xFF) == 0x00 && successBytes == 7) {
+                } else if ((nextRead[0] & 0xFF) == 0x00 && successBytes == 7) {
                     return i - 7;
                 } else {
                     successBytes = 0;
@@ -79,7 +79,7 @@ public class Cabfile {
     private CFHeader getHeader() throws CabException {
         CFHeader cfHeader = new CFHeader(readBytes);
         cfHeader.populate(archiveStream);
-        readBytes += (long) cfHeader.getStructureSize();
+        readBytes += cfHeader.getStructureSize();
 
         return cfHeader;
     }
@@ -87,21 +87,21 @@ public class Cabfile {
     public CFFolder getFolder() throws CabException {
         CFFolder cfFolder = new CFFolder(readBytes);
         cfFolder.populate(archiveStream);
-        readBytes += (long) cfFolder.getStructureSize();
+        readBytes += cfFolder.getStructureSize();
         return cfFolder;
     }
 
     public CFFile getFile() throws CabException {
         CFFile cfFile = new CFFile(readBytes);
         cfFile.populate(archiveStream);
-        readBytes += (long) cfFile.getStructureSize();
+        readBytes += cfFile.getStructureSize();
         return cfFile;
     }
 
-    public CFData getData(CompressionType compressionType) throws CabException {
-        CFData cfData = new CFData(readBytes, compressionType);
+    public CFData getData() throws CabException {
+        CFData cfData = new CFData(readBytes);
         cfData.populate(archiveStream);
-        readBytes += (long) cfData.getStructureSize();
+        readBytes += cfData.getStructureSize();
         return cfData;
     }
 
@@ -117,67 +117,56 @@ public class Cabfile {
     public static void main(String[] args) throws CabException, IOException {
         File tahoma32 = new File("/Users/tinou/Downloads/arial32.exe");
         Cabfile cabfile = new Cabfile(tahoma32);
-        int offset = cabfile.findCabOffset();
-        //System.out.println(offset);
+        cabfile.findCabOffset();
+        // System.out.println(offset);
         CFHeader header = cabfile.getHeader();
 
         Collection<CFFolder> cfFolders = new ArrayList<>();
-        for(int i = 0; i < header.getNumberOfFolders(); i++) {
+        for (int i = 0; i < header.getNumberOfFolders(); i++) {
             CFFolder cfFolder = cabfile.getFolder();
             cfFolders.add(cfFolder);
-            //System.out.println(cfFolder);
+            // System.out.println(cfFolder);
         }
-
 
         cabfile.jumpTo(header.coffFiles[0]);
 
         Collection<CFFile> cfiles = new ArrayList<>();
-        for(int i = 0; i < header.getNumberOfFiles(); i++) {
+        for (int i = 0; i < header.getNumberOfFiles(); i++) {
             CFFile cfile = cabfile.getFile();
             cfiles.add(cfile);
-            //System.out.println(cfile);
+            // System.out.println(cfile);
         }
 
-        CFData cfData = cabfile.getData(CompressionType.NONE);
-        //System.out.println(cfData);
-        //System.out.println(Hex.encodeHexString(cfData.ab));
+        cabfile.getData();
 
-        CFData cfData2 = cabfile.getData(CompressionType.NONE);
-        //System.out.println(cfData2);
-        //System.out.println(Hex.encodeHexString(cfData2.ab));
-
+        cabfile.getData();
 
         /*
-        for(CFFile cfFile: cfiles) {
-            if("fontinst.inf".equals(cfFile.getFilename())) {
-                CFFolder cfFolder = cfFolders.get((int) cfFile.getFolderIndex());
-
-
-                long dataIndex = cfFolder.getOffsetStartData() + cfFile.getOffsetStartDataInsideFolder();
-
-                System.out.println("Index: ");
-                System.out.println(dataIndex);
-
-                cabfile.jumpTo(dataIndex);
-
-
-                CFData cfData = cabfile.getData();
-
-
-                cfFolder.dump();
-                System.out.println(cfFile);
-                System.out.println(cfData);
-
-                File test = new File("/tmp/test");
-                FileOutputStream stream = new FileOutputStream(test);
-
-                stream.write(cfData.ab);
-                System.out.println(Arrays.toString(cfData.ab));
-            }
-        } */
+         * for(CFFile cfFile: cfiles) {
+         * if("fontinst.inf".equals(cfFile.getFilename())) { CFFolder cfFolder =
+         * cfFolders.get((int) cfFile.getFolderIndex());
+         *
+         *
+         * long dataIndex = cfFolder.getOffsetStartData() +
+         * cfFile.getOffsetStartDataInsideFolder();
+         *
+         * System.out.println("Index: "); System.out.println(dataIndex);
+         *
+         * cabfile.jumpTo(dataIndex);
+         *
+         *
+         * CFData cfData = cabfile.getData();
+         *
+         *
+         * cfFolder.dump(); System.out.println(cfFile);
+         * System.out.println(cfData);
+         *
+         * File test = new File("/tmp/test"); FileOutputStream stream = new
+         * FileOutputStream(test);
+         *
+         * stream.write(cfData.ab);
+         * System.out.println(Arrays.toString(cfData.ab)); } }
+         */
     }
-
-
-
 
 }
