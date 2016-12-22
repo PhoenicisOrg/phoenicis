45aef0ee2dcd0b36fcaf041c3abbfe4d466059df
Quentin PÂRIS <git@quentin.paris>
2016-12-22 15:03:57 +0100

    Library

---
+++ phoenicis-library/src/main/java/com/phoenicis/library/LibraryConfiguration.java
@@ -0,0 +1,7 @@
+package com.phoenicis.library;
+
+/**
+ * Created by qparis on 22/12/2016.
+ */
+public class LibraryConfiguration {
+}
---
+++ phoenicis-library/src/main/java/com/phoenicis/library/LibraryManager.java
@@ -0,0 +1,65 @@
+package com.phoenicis.library;
+
+import com.phoenicis.library.dto.ShortcutDTO;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LibraryManager {
+    private final String shortcutDirectory;
+
+    public LibraryManager(String shortcutDirectory) {
+        this.shortcutDirectory = shortcutDirectory;
+    }
+
+    public List<ShortcutDTO> fetchShortcuts() {
+        final File shortcutDirectory = new File(this.shortcutDirectory);
+
+        if(!shortcutDirectory.exists()) {
+            return Collections.emptyList();
+        }
+
+        final File[] directoryContent = shortcutDirectory.listFiles();
+        if(directoryContent == null) {
+            return Collections.emptyList();
+        }
+
+        final List<ShortcutDTO> shortcuts = new ArrayList<>();
+        for (File file : directoryContent) {
+            if("shortcut".equals(FilenameUtils.getExtension(file.getName()))) {
+                shortcuts.add(fetchShortcutDTO(shortcutDirectory, file));
+            }
+        }
+
+        return shortcuts;
+    }
+
+    private ShortcutDTO fetchShortcutDTO(File shortcutDirectory, File file) {
+        final String baseName = FilenameUtils.getBaseName(file.getName());
+        final File iconFile = new File(shortcutDirectory, baseName + ".icon");
+        final File miniatureFile = new File(shortcutDirectory, baseName + ".miniature");
+        final File descriptionFile = new File(shortcutDirectory, baseName + ".description");
+
+        try {
+            byte[] icon = IOUtils.toByteArray(iconFile.exists() ? new FileInputStream(iconFile) : getClass().getResourceAsStream("playonlinux.png"));
+            byte[] miniature = IOUtils.toByteArray(iconFile.exists() ? new FileInputStream(miniatureFile) : getClass().getResourceAsStream("defaultMiniature.png"));
+            String description = descriptionFile.exists() ? IOUtils.toString(new FileInputStream(descriptionFile), "UTF-8") : "";
+            return new ShortcutDTO.Builder()
+                    .withName(baseName)
+                    .withScript(IOUtils.toString(new FileInputStream(file), "UTF-8"))
+                    .withIcon(icon)
+                    .withMiniature(miniature)
+                    .build();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+
+    }
+}
