be1ff452eb0d86a5746b5ead8d2ce7bfd3934e0b
Quentin PÂRIS <git@quentin.paris>
2016-12-23 00:44:44 +0100

    Ability to read shortcuts

--- phoenicis-library/src/main/java/com/phoenicis/library/LibraryConfiguration.java
+++ phoenicis-library/src/main/java/com/phoenicis/library/LibraryConfiguration.java
@@ -1,11 +1,16 @@
 package com.phoenicis.library;
 
+import com.playonlinux.scripts.ScriptsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LibraryConfiguration {
+    @Autowired
+    private ScriptsConfiguration scriptsConfiguration;
+
     @Value("${application.user.shortcuts}")
     private String shortcutDirectory;
 
@@ -21,6 +26,6 @@ public class LibraryConfiguration {
 
     @Bean
     public ShortcutRunner shortcutRunner() {
-        return new ShortcutRunner();
+        return new ShortcutRunner(scriptsConfiguration.scriptInterpreter());
     }
 }
--- phoenicis-library/src/main/java/com/phoenicis/library/ShortcutRunner.java
+++ phoenicis-library/src/main/java/com/phoenicis/library/ShortcutRunner.java
@@ -1,10 +1,40 @@
 package com.phoenicis.library;
 
 import com.phoenicis.library.dto.ShortcutDTO;
+import com.playonlinux.scripts.interpreter.InteractiveScriptSession;
+import com.playonlinux.scripts.interpreter.ScriptInterpreter;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 public class ShortcutRunner {
+    private final ScriptInterpreter scriptInterpreter;
+
+    public ShortcutRunner(ScriptInterpreter scriptInterpreter) {
+        this.scriptInterpreter = scriptInterpreter;
+    }
+
     public void run(ShortcutDTO shortcutDTO) {
+        final InteractiveScriptSession interactiveScriptSession = scriptInterpreter.createInteractiveSession();
+
+        interactiveScriptSession.eval("include([\"Functions\", \"Shortcuts\", \"Reader\"]);",
+                ignored -> {
+                    interactiveScriptSession.eval(
+                            "new ShortcutReader()",
+                            output -> {
+                                final ScriptObjectMirror shortcutReader = (ScriptObjectMirror) output;
+                                shortcutReader.callMember("of", shortcutDTO.getScript());
+                                shortcutReader.callMember("run");
+                            },
+                            this::throwError
+                    );
+                },
+                this::throwError
+        );
+
         System.out.println("I will run");
         System.out.println(shortcutDTO.getScript());
+    }
+
+    private void throwError(Exception e) {
+        throw new IllegalStateException(e);
     }
 }
