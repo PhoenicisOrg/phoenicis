e05b854e8b822a6089c5d397e260472b7880ffaf
Philipp Hoppermann <plata@mailbox.org>
2016-12-26 18:21:52 +0100

    removed outdated examples

--- samples/javaEcho.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[ "$PLAYONLINUX" = "" ] && exit 0
-source "$PLAYONLINUX/lib/sources"
-
-javaecho "Test"
-exit
--- samples/scriptExampleCustomTemplate.py
+++
@@ -1,9 +0,0 @@
-from com.playonlinux.framework.templates import WineSteamInstaller
-
-class Example(WineSteamInstaller):
-    logContext = "SteamExample"
-    title = "Example with Steam"
-    prefix = "Prefix"
-    wineversion = "1.7.34"
-    steamId = 130
-    packages = ["package1", "package2"]
