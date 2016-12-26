c92542b7aa6bd95f99cac013c604358c937c9b93
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2016-12-26 14:00:45 -0500

    No need for this hacky workaround
    Should be up to users to set their default java version

--- phoenicis-dist/src/scripts/PlayOnLinux.sh
+++ phoenicis-dist/src/scripts/PlayOnLinux.sh
@@ -2,8 +2,4 @@
 POL_HOME=$(dirname $0)
 CLASSPATH=${CLASSPATH}:$POL_HOME/lib/*
 
-# Ensure path of Java 8 takes precedence on systems with Java 7 and 8 both installed
-# This is non-destructive and does not force users to set the default java env to Java 8
-export PATH=/usr/lib/jvm/java-8-openjdk/jre/bin/:$PATH
-
 java -classpath "$CLASSPATH" com.playonlinux.javafx.JavaFXApplication "$@"
