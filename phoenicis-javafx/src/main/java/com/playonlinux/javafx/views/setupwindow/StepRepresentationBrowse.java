b89baeb507025813523c031c129a51f08034044b
Quentin PÂRIS <git@quentin.paris>
2017-01-02 23:29:14 +0100

    A better browse setup window

--- phoenicis-javafx/src/main/java/com/playonlinux/javafx/views/setupwindow/StepRepresentationBrowse.java
+++ phoenicis-javafx/src/main/java/com/playonlinux/javafx/views/setupwindow/StepRepresentationBrowse.java
@@ -19,7 +19,11 @@
 package com.playonlinux.javafx.views.setupwindow;
 
 import com.playonlinux.scripts.ui.Message;
-import javafx.scene.control.Button;
+import javafx.scene.Node;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -27,28 +31,62 @@ import java.util.List;
 
 import static com.playonlinux.configuration.localisation.Localisation.translate;
 
-public class StepRepresentationBrowse extends StepRepresentationMessage {
+public class StepRepresentationBrowse extends AbstractStepRepresentationWithHeader {
     private final File browseDirectory;
     private final Message<String> message;
+    private final String textToShow;
     private List<String> extensions;
     private File selectedFile;
 
     public StepRepresentationBrowse(SetupWindowJavaFXImplementation parent, Message<String> message,
                                     String textToShow, File browseDirectory, List<String> extensions) {
-        super(parent, message, textToShow);
+        super(parent, message);
         this.browseDirectory = browseDirectory;
         this.extensions = extensions;
         this.message = message;
+        this.textToShow = textToShow;
     }
 
     @Override
     public void drawStepContent() {
-        super.drawStepContent();
+        this.addToContentPanel(dragPane());
+        this.setNextButtonEnabled(false);
+    }
 
-        final  Button browseButton = new Button(translate("Browse"));
-        browseButton.setLayoutX(200);
-        browseButton.setLayoutY(100);
-        browseButton.setOnMouseClicked(event -> {
+    private Node dragPane() {
+        final Text textLabel = new Text(textToShow);
+        final Text dragLabel = new Text("Please click here, or drag a file to me.");
+        textLabel.getStyleClass().add("boldLabel");
+        dragLabel.getStyleClass().addAll("normalLabel");
+
+        final VBox dragTarget = new VBox();
+        dragTarget.getChildren().addAll(textLabel, dragLabel);
+        dragTarget.setOnDragOver(event -> {
+            if (event.getGestureSource() != dragTarget && event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+            event.consume();
+        });
+
+        dragTarget.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasFiles()) {
+                this.selectFile(db.getFiles().get(0));
+                success = true;
+            }
+
+            event.setDropCompleted(success);
+
+            event.consume();
+        });
+
+        dragTarget.setLayoutX(30);
+        dragTarget.setLayoutY(20);
+        dragTarget.setPrefSize(660, 308);
+        dragTarget.getStyleClass().addAll("dragAndDropBox");
+
+        dragTarget.setOnMouseClicked(event -> {
             final FileChooser fileChooser = new FileChooser();
             if(extensions != null) {
                 fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter(translate("Allowed files"), extensions));
@@ -57,13 +95,18 @@ public class StepRepresentationBrowse extends StepRepresentationMessage {
 
             File dialogResult = fileChooser.showOpenDialog(null);
             if(dialogResult != null){
-                selectedFile = dialogResult;
-                setNextButtonEnabled(true);
+                selectFile(dialogResult);
             }
         });
 
-        this.addToStep(browseButton);
-        this.setNextButtonEnabled(false);
+
+        return dragTarget;
+    }
+
+    private void selectFile(File selectedFile) {
+        if(selectedFile != null) {
+            message.send(selectedFile.getAbsolutePath());
+        }
     }
 
     @Override
