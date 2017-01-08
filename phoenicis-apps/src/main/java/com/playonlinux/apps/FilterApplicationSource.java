8afae11e88c334fa12bc167234e076b5a967fd97
Quentin PÂRIS <git@quentin.paris>
2017-01-08 18:08:41 +0100

    Allow enforce testing application to be shown

--- phoenicis-apps/src/main/java/com/playonlinux/apps/FilterApplicationSource.java
+++ phoenicis-apps/src/main/java/com/playonlinux/apps/FilterApplicationSource.java
@@ -15,37 +15,44 @@ import java.util.stream.Collectors;
  */
 class FilterApplicationSource implements ApplicationsSource {
     private final ApplicationsSource applicationsSource;
-    private final OperatingSystemFetcher operatinSystemFetcher;
+    private final OperatingSystemFetcher operatingSystemFetcher;
+    private final boolean enforceIncompatibleOperatingSystems;
 
     FilterApplicationSource(ApplicationsSource applicationsSource,
-                            OperatingSystemFetcher operatinSystemFetcher) {
+                            OperatingSystemFetcher operatingSystemFetcher,
+                            boolean enforceIncompatibleOperatingSystems) {
         this.applicationsSource = applicationsSource;
-        this.operatinSystemFetcher = operatinSystemFetcher;
+        this.operatingSystemFetcher = operatingSystemFetcher;
+        this.enforceIncompatibleOperatingSystems = enforceIncompatibleOperatingSystems;
     }
 
     @Override
     public List<CategoryDTO> fetchInstallableApplications() {
-        final OperatingSystem currentOperatingSystem =operatinSystemFetcher.fetchCurrentOperationSystem();
+        final OperatingSystem currentOperatingSystem = operatingSystemFetcher.fetchCurrentOperationSystem();
         final List<CategoryDTO> categories = applicationsSource.fetchInstallableApplications();
         final List<CategoryDTO> filteredCategories = new ArrayList<>();
         for (CategoryDTO category : categories) {
             final List<ApplicationDTO> applications = new ArrayList<>();
             for (ApplicationDTO application : category.getApplications()) {
-                final List<ScriptDTO> scripts = application.getScripts().stream().filter(script -> script.getCompatibleOperatingSystems() == null || script.getCompatibleOperatingSystems().contains(currentOperatingSystem)).collect(Collectors.toList());
-
-                if(!scripts.isEmpty()) {
+                final List<ScriptDTO> scripts;
+                if(enforceIncompatibleOperatingSystems) {
+                    scripts = application.getScripts();
+                } else {
+                    scripts = application.getScripts().stream().filter(script -> script.getCompatibleOperatingSystems() == null || script.getCompatibleOperatingSystems().contains(currentOperatingSystem)).collect(Collectors.toList());
+                }
+                if (!scripts.isEmpty()) {
                     applications.add(
                             new ApplicationDTO.Builder(application)
-                                .withScripts(scripts)
-                                .build()
+                                    .withScripts(scripts)
+                                    .build()
                     );
                 }
             }
-            if(!applications.isEmpty()) {
+            if (!applications.isEmpty()) {
                 filteredCategories.add(
                         new CategoryDTO.Builder(category)
-                            .withApplications(applications)
-                            .build()
+                                .withApplications(applications)
+                                .build()
                 );
             }
         }
