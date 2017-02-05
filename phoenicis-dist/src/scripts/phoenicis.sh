727aca87663abbb06f9285ad8b557ab5523817be
Plata <plata@mailbox.org>
2017-02-05 10:02:58 +0100

    fixed #558

--- phoenicis-dist/src/scripts/phoenicis.sh
+++ phoenicis-dist/src/scripts/phoenicis.sh
@@ -2,4 +2,4 @@
 POL_HOME=$(dirname $0)
 CLASSPATH=${CLASSPATH}:$POL_HOME/lib/*
 
-java -classpath "$CLASSPATH" JavaFXApplication "$@"
+java -classpath "$CLASSPATH" org.phoenicis.javafx.JavaFXApplication "$@"
