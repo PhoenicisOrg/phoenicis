d6e19e0b9d76f29d25d8245ebf4a67a058225e8c
Plata <plata@mailbox.org>
2017-02-11 15:15:42 +0100

    fixed themed category icons
    The previous solution did not work properly because setStyle always overrides the CSS.

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/apps/ViewApps.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/apps/ViewApps.java
@@ -18,6 +18,7 @@
 
 package org.phoenicis.javafx.views.mainwindow.apps;
 
+import com.google.common.collect.Sets;
 import org.phoenicis.apps.dto.ApplicationDTO;
 import org.phoenicis.apps.dto.CategoryDTO;
 import org.phoenicis.apps.dto.ScriptDTO;
@@ -36,6 +37,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import static org.phoenicis.configuration.localisation.Localisation.translate;
@@ -79,13 +81,28 @@ public class ViewApps extends MainWindowView {
      */
     public void populate(List<CategoryDTO> categories) {
         Platform.runLater(() -> {
+            Set<String> themedCategories = Sets.newHashSet(
+                    "custom",
+                    "development",
+                    "games",
+                    "graphics",
+                    "internet",
+                    "multimedia",
+                    "office",
+                    "other",
+                    "science"
+            );
+
             final List<LeftButton> leftButtonList = new ArrayList<>();
             for (CategoryDTO category : categories) {
                 if(category.getType() == CategoryDTO.CategoryType.INSTALLERS) {
                     final LeftButton categoryButton = new LeftButton(category.getName());
-                    final String themeName = new String(category.getName().toLowerCase() + "Button");
-                    categoryButton.setStyle("-fx-background-image: url('" + category.getIcon() + "');");
-                    categoryButton.getStyleClass().add(themeName);
+                    if (themedCategories.contains(category.getName().toLowerCase())) {
+                        final String themeName = new String(category.getName().toLowerCase() + "Button");
+                        categoryButton.getStyleClass().add(themeName);
+                    } else {
+                        categoryButton.setStyle("-fx-background-image: url('" + category.getIcon() + "');");
+                    }
                     categoryButton.setOnMouseClicked(event -> selectCategory(category));
                     leftButtonList.add(categoryButton);
                 }
