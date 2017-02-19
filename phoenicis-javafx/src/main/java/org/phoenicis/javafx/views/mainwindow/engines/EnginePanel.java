68e4ffcda4718b7b2411ed1d73f529fb9db1d7a2
Plata <plata@mailbox.org>
2017-02-19 18:19:35 +0100

    implemented WineEngineDTO
    New DTO containing the information required to install/delete a wine engine.

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/engines/EnginePanel.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/engines/EnginePanel.java
@@ -21,7 +21,7 @@ package org.phoenicis.javafx.views.mainwindow.engines;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
-import org.phoenicis.engines.dto.WineVersionDTO;
+import org.phoenicis.engines.dto.WineEngineDTO;
 import org.phoenicis.javafx.views.common.ErrorMessage;
 import org.phoenicis.javafx.views.common.TextWithStyle;
 import org.slf4j.Logger;
@@ -36,10 +36,10 @@ final class EnginePanel extends VBox {
     private static final String CONFIGURATION_PANE_CSS_CLASS = "containerConfigurationPane";
     private final Logger LOGGER = LoggerFactory.getLogger(EnginePanel.class);
 
-    private Consumer<WineVersionDTO> onEngineInstall = (engine) -> {};
-    private Consumer<WineVersionDTO> onEngineDelete = (engine) -> {};
+    private Consumer<WineEngineDTO> onEngineInstall = (engine) -> {};
+    private Consumer<WineEngineDTO> onEngineDelete = (engine) -> {};
 
-    public EnginePanel(WineVersionDTO wineVersionDTO) {
+    public EnginePanel(WineEngineDTO wineEngineDTO) {
         super();
 
         getStyleClass().add(CONFIGURATION_PANE_CSS_CLASS);
@@ -48,17 +48,17 @@ final class EnginePanel extends VBox {
         informationContentPane.getStyleClass().add("grid");
 
         informationContentPane.add(new TextWithStyle(translate("Version:"), CAPTION_TITLE_CSS_CLASS), 0, 0);
-        Label name = new Label(wineVersionDTO.getVersion());
+        Label name = new Label(wineEngineDTO.getVersion());
         name.setWrapText(true);
         informationContentPane.add(name, 1, 0);
 
         informationContentPane.add(new TextWithStyle(translate("Mono:"), CAPTION_TITLE_CSS_CLASS), 0, 1);
-        Label path = new Label(wineVersionDTO.getMonoFile());
+        Label path = new Label(wineEngineDTO.getMonoFile());
         path.setWrapText(true);
         informationContentPane.add(path, 1, 1);
 
         informationContentPane.add(new TextWithStyle(translate("Gecko:"), CAPTION_TITLE_CSS_CLASS), 0, 2);
-        Label version = new Label(wineVersionDTO.getGeckoFile());
+        Label version = new Label(wineEngineDTO.getGeckoFile());
         version.setWrapText(true);
         informationContentPane.add(version, 1, 2);
 
@@ -68,7 +68,7 @@ final class EnginePanel extends VBox {
         Button installButton = new Button("Install");
         installButton.setOnMouseClicked(evt -> {
             try {
-                onEngineInstall.accept(wineVersionDTO);
+                onEngineInstall.accept(wineEngineDTO);
             } catch (IllegalArgumentException e) {
                 LOGGER.error("Failed to get engine", e);
                 new ErrorMessage("Error while trying to install the engine", e).show();
@@ -78,7 +78,7 @@ final class EnginePanel extends VBox {
         Button deleteButton = new Button("Delete");
         deleteButton.setOnMouseClicked(evt -> {
             try {
-                onEngineDelete.accept(wineVersionDTO);
+                onEngineDelete.accept(wineEngineDTO);
             } catch (IllegalArgumentException e) {
                 LOGGER.error("Failed to get engine", e);
                 new ErrorMessage("Error while trying to delete the engine", e).show();
@@ -96,10 +96,10 @@ final class EnginePanel extends VBox {
         getChildren().addAll(informationContentPane, spacer, buttonBox);
     }
 
-    public void setOnEngineInstall(Consumer<WineVersionDTO> onEngineInstall) {
+    public void setOnEngineInstall(Consumer<WineEngineDTO> onEngineInstall) {
         this.onEngineInstall = onEngineInstall;
     }
-    public void setOnEngineDelete(Consumer<WineVersionDTO> onEngineDelete) {
+    public void setOnEngineDelete(Consumer<WineEngineDTO> onEngineDelete) {
         this.onEngineDelete = onEngineDelete;
     }
 }
