a688d392e5ae1a420419fd5ab4aadb11aed29a61
plata <plata@users.noreply.github.com>
2017-03-05 10:33:03 +0100

    applied code formatter (#636)

--- phoenicis-tools/src/main/java/org/phoenicis/tools/archive/cab/Cabfile.java
+++ phoenicis-tools/src/main/java/org/phoenicis/tools/archive/cab/Cabfile.java
@@ -172,5 +172,4 @@ public class Cabfile {
             }
         } */
     }
-
 }
--- phoenicis-tools/src/main/java/org/phoenicis/tools/win32/ExeAnalyser.java
+++ phoenicis-tools/src/main/java/org/phoenicis/tools/win32/ExeAnalyser.java
@@ -61,5 +61,4 @@ public class ExeAnalyser {
             return peFile.getArchitecture() == architecture;
         }
     }
-
 }
