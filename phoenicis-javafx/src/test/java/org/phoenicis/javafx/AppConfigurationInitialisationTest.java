0a3edfe694e1d4115e22ba97ea3805d9124faa93
madoar <madoar@users.noreply.github.com>
2017-03-20 21:42:26 +0100

    Added a test for the initialisation of the AppConfiguration class (#668)
    * - added a test for the initialisation of the AppConfiguration class
    
    * - added headless mode for javafx tests

---
+++ phoenicis-javafx/src/test/java/org/phoenicis/javafx/AppConfigurationInitialisationTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.phoenicis.javafx;
+
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.testfx.api.FxToolkit;
+
+/**
+ * @author marc
+ *
+ */
+public class AppConfigurationInitialisationTest {
+
+	@Test
+	public void testAppConfigurationInitialisation() throws TimeoutException {
+		FxToolkit.registerPrimaryStage();
+		FxToolkit.setupFixture(() -> new AnnotationConfigApplicationContext(AppConfiguration.class));
+	}
+
+}
