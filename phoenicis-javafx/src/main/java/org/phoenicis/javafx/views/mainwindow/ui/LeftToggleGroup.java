bee52ca03535db4788b7dab0ca507ccda687bcde
Marc Arndt <marc.arndt@masimi.de>
2017-03-30 15:19:39 +0200

    - changed LeftToggleGroup to use EasyBind instead of a custom solution

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/ui/LeftToggleGroup.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/ui/LeftToggleGroup.java
@@ -1,25 +1,28 @@
 package org.phoenicis.javafx.views.mainwindow.ui;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+import org.fxmisc.easybind.EasyBind;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 /**
  * Created by marc on 28.03.17.
  */
 public class LeftToggleGroup<E> extends LeftGroup {
+    private final ToggleGroup toggleGroup;
     private final ToggleButton allButton;
 
-    private final ToggleGroup toggleGroup;
+    private final VBox toggleButtonBox;
 
     private final ObservableList<E> elements;
+    private final ObservableList<? extends ToggleButton> mappedToggleButtons;
 
     public static <T> LeftToggleGroup<T> create(String name, Supplier<ToggleButton> allButtonSupplier, Function<T, ? extends ToggleButton> converter) {
         return new LeftToggleGroup<T>(name, allButtonSupplier, converter);
@@ -28,40 +31,27 @@ public class LeftToggleGroup<E> extends LeftGroup {
     private LeftToggleGroup(String name, Supplier<ToggleButton> allButtonSupplier, Function<E, ? extends ToggleButton> converter) {
         super(name);
 
-        this.elements = FXCollections.observableArrayList();
         this.toggleGroup = new ToggleGroup();
+        this.toggleButtonBox = new VBox();
+        this.toggleButtonBox.getStyleClass().add("leftPaneInside");
+
+        this.elements = FXCollections.observableArrayList();
+        this.mappedToggleButtons = EasyBind.map(this.elements, value -> {
+            ToggleButton button = converter.apply(value);
+
+            button.setToggleGroup(toggleGroup);
+
+            return button;
+        });
 
         this.allButton = allButtonSupplier.get();
         this.allButton.setToggleGroup(toggleGroup);
 
         ObservableList<Node> target = this.getChildren();
         target.add(allButton);
+        target.add(toggleButtonBox);
 
-        this.elements.addListener((ListChangeListener<? super E>) change -> {
-            while (change.next()) {
-                int from = change.getFrom();
-                int to = change.getTo();
-                if (change.wasPermutated()) {
-                    target.subList(from + 2, to + 2).clear();
-                    target.addAll(from + 2, elements.subList(from, to).stream().map(element -> {
-                        ToggleButton result = converter.apply(element);
-
-                        result.setToggleGroup(toggleGroup);
-
-                        return result;
-                    }).collect(Collectors.toList()));
-                } else {
-                    target.subList(from + 2, from + 2 + change.getRemovedSize()).clear();
-                    target.addAll(from + 2, elements.subList(from, from + change.getAddedSize()).stream().map(element -> {
-                        ToggleButton result = converter.apply(element);
-
-                        result.setToggleGroup(toggleGroup);
-
-                        return result;
-                    }).collect(Collectors.toList()));
-                }
-            }
-        });
+        Bindings.bindContent(this.toggleButtonBox.getChildren(), this.mappedToggleButtons);
     }
 
     public ObservableList<E> getElements() {
