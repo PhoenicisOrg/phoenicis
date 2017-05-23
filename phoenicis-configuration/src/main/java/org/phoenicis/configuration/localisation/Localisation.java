0e2c398b57b896bdf4d0ff0edac1ec6210c22e26
Plata <plata@mailbox.org>
2017-05-23 18:53:18 +0200

    provide static access to i18n

--- phoenicis-configuration/src/main/java/org/phoenicis/configuration/localisation/Localisation.java
+++ phoenicis-configuration/src/main/java/org/phoenicis/configuration/localisation/Localisation.java
@@ -18,13 +18,45 @@
 
 package org.phoenicis.configuration.localisation;
 
+import org.xnap.commons.i18n.I18n;
+import org.xnap.commons.i18n.I18nFactory;
+
+import java.util.Locale;
+
 public final class Localisation {
     // This is a static class
     private Localisation() {
 
     }
 
-    public static String translate(String stringToTranslate) {
-        return stringToTranslate;
+    private static I18n getI18n() {
+        I18n getI18n = I18nFactory.getI18n(Localisation.class, "Messages");
+        getI18n.setLocale(Locale.GERMAN);
+        return getI18n;
     }
+
+    public static String tr(String str) {
+        return getI18n().tr(str);
+    }
+
+    public static String tr(String text, Object o1) {
+        return getI18n().tr(text, o1);
+    }
+
+    public static String tr(String text, Object o1, Object o2) {
+        return getI18n().tr(text, o1, o2);
+    }
+
+    public static String tr(String text, Object o1, Object o2, Object o3) {
+        return getI18n().tr(text, o1, o2, o3);
+    }
+
+    public static String tr(String text, Object o1, Object o2, Object o3, Object o4) {
+        return getI18n().tr(text, o1, o2, o3, o4);
+    }
+
+    public static String tr(String text, Object[] objects) {
+        return getI18n().tr(text, objects);
+    }
+
 }
