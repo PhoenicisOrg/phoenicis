88924a854d0c076c8f51dd4bdeeaab9964c0374a
Quentin PÂRIS <git@quentin.paris>
2017-06-17 19:14:01 +0200

    Automatic translation of any object

---
+++ phoenicis-configuration/src/test/org/phoenicis/configuration/localisation/CollectionsOfTranslatableObject.java
@@ -0,0 +1,25 @@
+package org.phoenicis.configuration.localisation;
+
+import java.util.List;
+
+@Translatable
+public class CollectionsOfTranslatableObject {
+    private final List<String> itemToBeTranslated;
+    private final List<String> itemNotToBeTranslated;
+
+    @TranslatableCreator
+    public CollectionsOfTranslatableObject(@ParameterName("itemToBeTranslated") List<String> itemToBeTranslated,
+                                           @ParameterName("itemNotToBeTranslated") List<String> itemNotToBeTranslated) {
+        this.itemToBeTranslated = itemToBeTranslated;
+        this.itemNotToBeTranslated = itemNotToBeTranslated;
+    }
+
+    @Translate
+    public List<String> getItemToBeTranslated() {
+        return itemToBeTranslated;
+    }
+
+    public List<String> getItemNotToBeTranslated() {
+        return itemNotToBeTranslated;
+    }
+}
