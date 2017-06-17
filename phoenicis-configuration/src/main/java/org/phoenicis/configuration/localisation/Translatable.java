12240d0b54fe9102534c6ff6a3b9d026218d3113
Plata <plata@mailbox.org>
2017-06-17 12:25:53 +0200

    JavaDoc for Translatable

--- phoenicis-configuration/src/main/java/org/phoenicis/configuration/localisation/Translatable.java
+++ phoenicis-configuration/src/main/java/org/phoenicis/configuration/localisation/Translatable.java
@@ -1,5 +1,10 @@
 package org.phoenicis.configuration.localisation;
 
+/**
+ * interface for objects which can be translated via tr()
+ * e.g. implemented by the DTOs to translate application name and description
+ * @param <E>
+ */
 public interface Translatable<E extends Translatable<?>> {
 
     /**
