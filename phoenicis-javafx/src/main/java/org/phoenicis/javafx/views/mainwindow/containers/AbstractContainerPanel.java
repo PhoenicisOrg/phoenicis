a9586aea26aa49b81152fc1a041cede2821491b9
plata <plata@users.noreply.github.com>
2017-08-20 19:40:23 +0200

    show container name in title of details view (#1004)

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/containers/AbstractContainerPanel.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/containers/AbstractContainerPanel.java
@@ -19,17 +19,19 @@
 package org.phoenicis.javafx.views.mainwindow.containers;
 
 import javafx.scene.control.TabPane;
+import org.phoenicis.containers.dto.ContainerDTO;
 import org.phoenicis.engines.dto.EngineVersionDTO;
 import org.phoenicis.javafx.views.common.widgets.lists.DetailsView;
 
 import java.util.List;
 
-abstract class AbstractContainerPanel<T> extends DetailsView {
+abstract class AbstractContainerPanel<T extends ContainerDTO> extends DetailsView {
     protected final TabPane tabPane;
 
     AbstractContainerPanel(T containerEntity, List<EngineVersionDTO> engineVersions) {
         this.tabPane = new TabPane();
 
+        this.setTitle(containerEntity.getName());
         this.setCenter(tabPane);
     }
 }
