cc5012eae1364c7220f15be50e5b5509347533e3
Plata <plata@mailbox.org>
2017-08-21 19:44:52 +0200

    review comments

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/ui/LeftListWidgetChooser.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/ui/LeftListWidgetChooser.java
@@ -7,6 +7,7 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import org.phoenicis.javafx.views.common.widgets.lists.CombinedListWidget;
 import org.phoenicis.javafx.views.common.widgets.lists.ListWidgetType;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
@@ -18,7 +19,7 @@ import java.util.function.Consumer;
  * Created by marc on 15.05.17.
  */
 public class LeftListWidgetChooser<E> extends HBox {
-    private final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(LeftListWidgetChooser.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(LeftListWidgetChooser.class);
 
     private List<CombinedListWidget<E>> listWidgets;
 
@@ -28,6 +29,9 @@ public class LeftListWidgetChooser<E> extends HBox {
     private ToggleButton compactListButton;
     private ToggleButton detailsListButton;
 
+    /**
+     * consumer which is executed when a different the list type is chosen
+     */
     private Optional<Consumer<ListWidgetType>> onChoose;
 
     public LeftListWidgetChooser(CombinedListWidget<E> listWidget) {
@@ -67,20 +71,20 @@ public class LeftListWidgetChooser<E> extends HBox {
         this.compactListButton.setToggleGroup(toggleGroup);
         this.compactListButton.getStyleClass().addAll("listIcon", "compactList");
         this.compactListButton
-                .setOnAction(event -> listWidgets.forEach(widget -> {
-                    widget.showList(ListWidgetType.COMPACT_LIST);
+                .setOnAction(event -> {
+                    listWidgets.forEach(widget -> widget.showList(ListWidgetType.COMPACT_LIST));
                     this.onChoose.ifPresent(consumer -> consumer.accept(ListWidgetType.COMPACT_LIST));
-                }));
+                });
         this.compactListButton.addEventFilter(ActionEvent.ANY, filter);
 
         this.detailsListButton = new ToggleButton();
         this.detailsListButton.setToggleGroup(toggleGroup);
         this.detailsListButton.getStyleClass().addAll("listIcon", "detailsList");
         this.detailsListButton
-                .setOnAction(event -> listWidgets.forEach(widget -> {
-                    widget.showList(ListWidgetType.DETAILS_LIST);
+                .setOnAction(event -> {
+                    listWidgets.forEach(widget -> widget.showList(ListWidgetType.DETAILS_LIST));
                     this.onChoose.ifPresent(consumer -> consumer.accept(ListWidgetType.DETAILS_LIST));
-                }));
+                });
         this.detailsListButton.addEventFilter(ActionEvent.ANY, filter);
 
         this.getChildren().setAll(iconsListButton, compactListButton, detailsListButton);
