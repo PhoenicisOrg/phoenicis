0f29648e9e3c34609599d5acc4047b57cf8b42db
Plata <plata@mailbox.org>
2017-08-24 19:36:53 +0200

    formatter

--- phoenicis-javafx/src/test/java/org/phoenicis/javafx/views/common/PhoenicisFilteredListTest.java
+++ phoenicis-javafx/src/test/java/org/phoenicis/javafx/views/common/PhoenicisFilteredListTest.java
@@ -20,8 +20,8 @@ public class PhoenicisFilteredListTest {
         PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(observableList, i -> i % 2 == 0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
     }
 
     @Test
@@ -30,22 +30,22 @@ public class PhoenicisFilteredListTest {
         PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(observableList, i -> i % 2 == 0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         observableList.add(0);
 
         assertEquals(3, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
-        assertEquals(0, (int)filteredList.get(2));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
+        assertEquals(0, (int) filteredList.get(2));
 
         observableList.add(5);
 
         assertEquals(3, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
-        assertEquals(0, (int)filteredList.get(2));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
+        assertEquals(0, (int) filteredList.get(2));
     }
 
     @Test
@@ -54,19 +54,19 @@ public class PhoenicisFilteredListTest {
         PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(observableList, i -> i % 2 == 0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         observableList.remove(0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         observableList.remove(0);
 
         assertEquals(1, filteredList.size());
-        assertEquals(4, (int)filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(0));
     }
 
     @Test
@@ -75,19 +75,19 @@ public class PhoenicisFilteredListTest {
         PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(observableList, i -> i % 2 == 0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         observableList.set(1, 1);
 
         assertEquals(1, filteredList.size());
-        assertEquals(4, (int)filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(0));
 
         observableList.set(3, 0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(4, (int)filteredList.get(0));
-        assertEquals(0, (int)filteredList.get(1));
+        assertEquals(4, (int) filteredList.get(0));
+        assertEquals(0, (int) filteredList.get(1));
     }
 
     @Test
@@ -97,14 +97,14 @@ public class PhoenicisFilteredListTest {
         PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(sortedList, i -> i % 2 == 0);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         sortedList.comparatorProperty().set(Comparator.comparing(String::valueOf).reversed());
 
         assertEquals(2, filteredList.size());
-        assertEquals(4, (int)filteredList.get(0));
-        assertEquals(2, (int)filteredList.get(1));
+        assertEquals(4, (int) filteredList.get(0));
+        assertEquals(2, (int) filteredList.get(1));
     }
 
     private int moduloResult = 0;
@@ -112,23 +112,24 @@ public class PhoenicisFilteredListTest {
     @Test
     public void testTrigger() {
         ObservableList<Integer> observableList = FXCollections.observableArrayList(Arrays.asList(1, 2, 4, 3));
-        PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(observableList, i -> i % 2 == moduloResult);
+        PhoenicisFilteredList<Integer> filteredList = new PhoenicisFilteredList<>(observableList,
+                i -> i % 2 == moduloResult);
 
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         this.moduloResult = 1;
 
         // we should get the same result, because we didn't call trigger yet
         assertEquals(2, filteredList.size());
-        assertEquals(2, (int)filteredList.get(0));
-        assertEquals(4, (int)filteredList.get(1));
+        assertEquals(2, (int) filteredList.get(0));
+        assertEquals(4, (int) filteredList.get(1));
 
         filteredList.trigger();
 
         assertEquals(2, filteredList.size());
-        assertEquals(1, (int)filteredList.get(0));
-        assertEquals(3, (int)filteredList.get(1));
+        assertEquals(1, (int) filteredList.get(0));
+        assertEquals(3, (int) filteredList.get(1));
     }
 }
