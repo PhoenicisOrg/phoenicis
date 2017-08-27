52c0916056ad99df9089131246e632ae29545102
plata <plata@users.noreply.github.com>
2017-08-27 18:52:59 +0200

    use same hover/selected colors for all list widgets (#1066)

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/common/widgets/lists/details/DetailsListWidget.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/common/widgets/lists/details/DetailsListWidget.java
@@ -43,7 +43,7 @@ public class DetailsListWidget<E> extends ListView<DetailsListElement<E>> implem
 
         this.setPrefWidth(0);
         this.setPrefHeight(0);
-        this.getStyleClass().add("detailsListWidget");
+        this.getStyleClass().addAll("listWidget", "detailsListWidget");
         this.setCellFactory(param -> new ListElementListCell<DetailsListElement<E>>());
 
         this.items = FXCollections.observableArrayList();
