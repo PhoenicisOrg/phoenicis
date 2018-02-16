cd4558a783839ca16c6ba35be0032bd1314735d9
Plata <plata@mailbox.org>
2018-02-16 13:34:16 +0100

    exclude whole i18n folder

--- check_formatted.sh
+++ check_formatted.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-diffs=$(git diff --name-only | grep -v i18n/keys.pot | wc -l)
+diffs=$(git diff --name-only | grep -v ^i18n/ | wc -l)
 if [ $diffs != 0 ]; then
   echo "Some files are not formatted!"
-  git diff --name-only | grep -v i18n/keys.pot
+  git diff --name-only | grep -v ^i18n/
   exit 1
 fi
 exit 0
