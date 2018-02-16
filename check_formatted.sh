6086d720f5eed62de144ac054b387abd1c831988
Plata <plata@mailbox.org>
2018-02-16 12:39:58 +0100

    Travis CI: check that all files are formatted

---
+++ check_formatted.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+diffs=`git diff --name-only | grep -v i18n/keys.pot | wc -l`
+if [ $diffs != 0 ]; then
+  echo "Some files are not formatted!"
+  git diff --name-only | grep -v i18n/keys.pot
+  exit 1
+fi
+exit 0
