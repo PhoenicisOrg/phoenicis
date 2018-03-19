6c9ea4bd4c5dea8ce90457e5e92da19100390f1e
plata <plata@users.noreply.github.com>
2018-03-19 19:21:54 +0100

    Travis CI: updated check format (#1199)
    - use formatter:validate instead of custom script
    - do not build before format check to save time

--- check_formatted.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-diffs=$(git diff --name-only | grep -v ^i18n/ | wc -l)
-if [ $diffs != 0 ]; then
-  echo "Some files are not formatted!"
-  git diff --name-only | grep -v ^i18n/
-  exit 1
-fi
-exit 0
