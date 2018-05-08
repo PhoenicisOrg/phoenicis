5d6862edee103d7d823f2a232e2f012dd90fa28d
Plata <plata@mailbox.org>
2018-05-08 10:26:25 +0200

    Update "run in container"
    open file chooser in container directory

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/containers/ContainerToolsTab.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/containers/ContainerToolsTab.java
@@ -60,6 +60,13 @@ public class ContainerToolsTab extends Tab {
             this.lockAll();
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle(tr("Choose executable"));
+
+            // open in container directory if it exists
+            File containerDir = new File(this.container.getPath());
+            if (containerDir.canRead()) {
+                fileChooser.setInitialDirectory(containerDir);
+            }
+
             File file = fileChooser.showOpenDialog(this.getContent().getScene().getWindow());
             if (file != null) {
                 containerEngineController.runInContainer(container, file.getAbsolutePath(), this::unlockAll,
