244fe4a56dfd1576a718bc5edaa478366237c1bb
Plata <plata@mailbox.org>
2018-11-21 20:35:17 +0100

    Fix Java 11 build

--- phoenicis-multithreading/src/main/java/module-info.java
+++ phoenicis-multithreading/src/main/java/module-info.java
@@ -4,5 +4,5 @@ module org.phoenicis.multithreading {
     requires commons.lang;
     requires slf4j.api;
     requires spring.context;
-    requires java.xml.ws.annotation;
+    requires java.annotation;
 }
