f8302dc7a95a82e15bde069a1a3a630e8184da2a
madoar <madoar@users.noreply.github.com>
2018-11-26 21:17:11 +0100

    Refactor the SearchBox UI component with MVC in mind (#1481)
    - refactor the SearchBox UI component with MVC in mind
    - move the javafx mvc packages to a common components package

---
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/control/ControlBase.java
@@ -0,0 +1,29 @@
+package org.phoenicis.javafx.components.control;
+
+import javafx.scene.control.Control;
+import javafx.scene.control.Skin;
+import org.phoenicis.javafx.components.skin.SkinBase;
+
+/**
+ * A base class for a JavaFX component control/model
+ *
+ * @param <C> The control/model class itself
+ * @param <S> The skin/view class belonging to the control
+ */
+public abstract class ControlBase<C extends ControlBase<C, S>, S extends SkinBase<C, S>> extends Control {
+
+    /**
+     * Creates a new skin/view object for the control
+     *
+     * @return The created view object
+     */
+    public abstract S createSkin();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Skin<?> createDefaultSkin() {
+        return createSkin();
+    }
+}
---
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/skin/SkinBase.java
@@ -0,0 +1,39 @@
+package org.phoenicis.javafx.components.skin;
+
+import org.phoenicis.javafx.components.control.ControlBase;
+
+/**
+ * A base class for a JavaFX component skin/view
+ *
+ * @param <C> The control/model class belonging to the skin
+ * @param <S> The skin/view class itself
+ */
+public abstract class SkinBase<C extends ControlBase<C, S>, S extends SkinBase<C, S>>
+        extends javafx.scene.control.SkinBase<C> {
+    /**
+     * Constructor for all SkinBase instances.
+     *
+     * @param control The control for which this Skin should attach to.
+     */
+    protected SkinBase(C control) {
+        super(control);
+
+        // initialise the skin
+        initialise();
+    }
+
+    /**
+     * Initialises the content of this skin.
+     * This method is performed before the behavior is created and initialised
+     */
+    public abstract void initialise();
+
+    /**
+     * Gets the control to which this skin is applied
+     *
+     * @return The control to which this skin is applied
+     */
+    public C getControl() {
+        return getSkinnable();
+    }
+}
