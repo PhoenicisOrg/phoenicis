420ae1efe06dcc09cab2be8fa120e7843a3dc114
Plata <plata@mailbox.org>
2018-12-01 16:59:07 +0100

    Run AppConfigurationInitialisationTest in a temporary application.user.root

--- phoenicis-javafx/src/test/java/org/phoenicis/javafx/AppConfigurationInitialisationTest.java
+++ phoenicis-javafx/src/test/java/org/phoenicis/javafx/AppConfigurationInitialisationTest.java
@@ -1,12 +1,12 @@
-/**
- *
- */
 package org.phoenicis.javafx;
 
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.testfx.api.FxToolkit;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -17,10 +17,19 @@ import java.util.concurrent.TimeoutException;
 public class AppConfigurationInitialisationTest {
 
     /**
+     * run the test in a temporary application root environment
+     */
+    private static void setupApplicationRoot() throws IOException {
+        Path tempDir = Files.createTempDirectory(AppConfigurationInitialisationTest.class.getName());
+        tempDir.toFile().deleteOnExit();
+        System.setProperty("application.user.root", tempDir.toString());
+    }
+
+    /**
      * ensures that the test can run in headless mode
      * see https://stackoverflow.com/questions/27403410/headless-testing-with-javafx-and-testfx
      */
-    public static void setupHeadless() {
+    private static void setupHeadless() {
         System.setProperty("testfx.robot", "glass");
         System.setProperty("testfx.headless", "true");
         System.setProperty("prism.order", "sw");
@@ -29,7 +38,8 @@ public class AppConfigurationInitialisationTest {
     }
 
     @Test
-    public void testAppConfigurationInitialisation() throws TimeoutException {
+    public void testAppConfigurationInitialisation() throws TimeoutException, IOException {
+        setupApplicationRoot();
         setupHeadless();
         FxToolkit.registerPrimaryStage();
         FxToolkit.setupFixture(() -> new AnnotationConfigApplicationContext(AppConfiguration.class));
