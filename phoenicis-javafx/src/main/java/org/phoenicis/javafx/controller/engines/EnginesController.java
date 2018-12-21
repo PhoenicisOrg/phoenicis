8d4d81c93158269024e56d52a9bc420cefbd55c2
madoar <madoar@users.noreply.github.com>
2018-12-21 17:56:34 +0100

    Refactor confirm message (#1607)
    - use builder pattern for the confirm dialogs
    - replace old ConfirmMessage usage with ConfirmDialog
    - remove ConfirmMessage class

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/controller/engines/EnginesController.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/controller/engines/EnginesController.java
@@ -25,7 +25,7 @@ import org.phoenicis.engines.EnginesManager;
 import org.phoenicis.engines.dto.EngineCategoryDTO;
 import org.phoenicis.engines.dto.EngineSubCategoryDTO;
 import org.phoenicis.javafx.controller.apps.AppsController;
-import org.phoenicis.javafx.views.common.ConfirmMessage;
+import org.phoenicis.javafx.dialogs.ConfirmDialog;
 import org.phoenicis.javafx.views.common.ErrorMessage;
 import org.phoenicis.javafx.views.common.ThemeManager;
 import org.phoenicis.javafx.views.mainwindow.engines.EnginesView;
@@ -90,37 +90,39 @@ public class EnginesController {
         });
 
         this.enginesView.setOnInstallEngine(engineDTO -> {
-            ConfirmMessage confirmMessage = new ConfirmMessage(
-                    tr("Install {0}", engineDTO.getVersion()),
-                    tr("Are you sure you want to install {0}?", engineDTO.getVersion()),
-                    this.enginesView.getContent().getScene().getWindow());
-            confirmMessage.setResizable(true);
-            confirmMessage.ask(() -> this.enginesManager.getEngine(engineDTO.getId(),
-                    engine -> {
+            final ConfirmDialog confirmMessage = ConfirmDialog.builder()
+                    .withTitle(tr("Install {0}", engineDTO.getVersion()))
+                    .withMessage(tr("Are you sure you want to install {0}?", engineDTO.getVersion()))
+                    .withOwner(enginesView.getContent().getScene().getWindow())
+                    .withResizable(true)
+                    .withYesCallback(() -> this.enginesManager.getEngine(engineDTO.getId(), engine -> {
                         engine.install(engineDTO.getSubCategory(), engineDTO.getVersion());
+
                         // invalidate cache and force view update to show installed version correctly
                         this.versionsCache.remove(engineDTO.getId());
                         this.forceViewUpdate();
-                    },
-                    e -> Platform.runLater(
-                            () -> new ErrorMessage("Error", e, this.enginesView).show())));
+                    }, e -> Platform.runLater(() -> new ErrorMessage("Error", e, this.enginesView).show())))
+                    .build();
+
+            confirmMessage.showAndCallback();
         });
 
         this.enginesView.setOnDeleteEngine(engineDTO -> {
-            ConfirmMessage confirmMessage = new ConfirmMessage(
-                    tr("Delete {0}", engineDTO.getVersion()),
-                    tr("Are you sure you want to delete {0}?", engineDTO.getVersion()),
-                    this.enginesView.getContent().getScene().getWindow());
-            confirmMessage.setResizable(true);
-            confirmMessage.ask(() -> this.enginesManager.getEngine(engineDTO.getId(),
-                    engine -> {
+            final ConfirmDialog confirmMessage = ConfirmDialog.builder()
+                    .withTitle(tr("Delete {0}", engineDTO.getVersion()))
+                    .withMessage(tr("Are you sure you want to delete {0}?", engineDTO.getVersion()))
+                    .withOwner(enginesView.getContent().getScene().getWindow())
+                    .withResizable(true)
+                    .withYesCallback(() -> this.enginesManager.getEngine(engineDTO.getId(), engine -> {
                         engine.delete(engineDTO.getSubCategory(), engineDTO.getVersion());
+
                         // invalidate cache and force view update to show deleted version correctly
                         this.versionsCache.remove(engineDTO.getId());
                         this.forceViewUpdate();
-                    },
-                    e -> Platform.runLater(
-                            () -> new ErrorMessage("Error", e, this.enginesView).show())));
+                    }, e -> Platform.runLater(() -> new ErrorMessage("Error", e, this.enginesView).show())))
+                    .build();
+
+            confirmMessage.showAndCallback();
         });
     }
 
