99773c6fef3e9e1eb9c44fb4cb86717e692e00d6
Marc Arndt <marc.arndt@masimi.de>
2018-12-22 13:17:27 +0100

    - remove the ListElementListCell and DummyListElement classes

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/common/widgets/details/skin/DetailsListWidgetSkin.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/common/widgets/details/skin/DetailsListWidgetSkin.java
@@ -2,12 +2,12 @@ package org.phoenicis.javafx.components.common.widgets.details.skin;
 
 import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import org.phoenicis.javafx.collections.MappedList;
+import org.phoenicis.javafx.components.common.skin.SkinBase;
 import org.phoenicis.javafx.components.common.widgets.details.control.DetailsListElement;
 import org.phoenicis.javafx.components.common.widgets.details.control.DetailsListWidget;
-import org.phoenicis.javafx.components.common.skin.SkinBase;
-import org.phoenicis.javafx.views.common.widgets.lists.ListElementListCell;
 import org.phoenicis.javafx.components.common.widgets.utils.ListWidgetElement;
 import org.phoenicis.javafx.components.common.widgets.utils.ListWidgetSelection;
 
@@ -51,7 +51,24 @@ public class DetailsListWidgetSkin<E> extends SkinBase<DetailsListWidget<E>, Det
         container.setPrefWidth(0);
         container.setPrefHeight(0);
 
-        container.setCellFactory(param -> new ListElementListCell<>());
+        // ensure that empty rows have the same height as non-empty ones
+        container.setCellFactory(param -> {
+            final ListCell<DetailsListElement<E>> listCell = new ListCell<DetailsListElement<E>>() {
+                @Override
+                public void updateItem(DetailsListElement<E> item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (!empty && item != null) {
+                        setGraphic(item);
+                    } else {
+                        setGraphic(null);
+                    }
+                }
+            };
+
+            listCell.getStyleClass().addAll("detailsListElement");
+
+            return listCell;
+        });
 
         // ensure that updates to the selected element property are automatically reflected in the view
         getControl().selectedElementProperty().addListener((observable, oldValue, newValue) -> {
