6cf7c7d08e53deee117a387d3695bf2a9ded6f48
ovari <17465872+ovari@users.noreply.github.com>
2018-12-24 10:21:08 +1100

    Update ChecksumCalculator.java

--- phoenicis-tools/src/main/java/org/phoenicis/tools/checksum/ChecksumCalculator.java
+++ phoenicis-tools/src/main/java/org/phoenicis/tools/checksum/ChecksumCalculator.java
@@ -36,7 +36,7 @@ import static org.phoenicis.configuration.localisation.Localisation.tr;
 @Safe
 public class ChecksumCalculator {
     private static final int BLOCK_SIZE = 2048;
-    private static final String WAIT_MESSAGE = tr("Verifying the file ...");
+    private static final String WAIT_MESSAGE = tr("Verifying the file…");
 
     public String calculate(String fileToCheck, String algorithm, Consumer<ProgressEntity> onChange)
             throws IOException {
