c775ac09225778f5c64bea4ef8916c9f78bdbc65
madoar <madoar@users.noreply.github.com>
2018-12-29 18:18:02 +0100

    Refactor the MappedList class (#1669)
    - refactor the MappedList class
    - change the mapper function to be changeable at runtime
    - add additional test cases for changes to the mapper function
    - add documentation/javadoc

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/collections/MappedList.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/collections/MappedList.java
@@ -1,37 +1,118 @@
 package org.phoenicis.javafx.collections;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
 import org.phoenicis.javafx.collections.change.InitialisationChange;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
- * Created by marc on 01.04.17.
+ * An implementation of a mapped {@link ObservableList}, which maps the values of the source {@link ObservableList} into
+ * values of the target type {@link E}.
+ *
+ * @param <E> The instance type of the target elements
+ * @param <F> The instance type of the source elements
  */
 public class MappedList<E, F> extends PhoenicisTransformationList<E, F> {
-    private final Function<? super F, ? extends E> mapper;
+    /**
+     * The mapper function used to map the source values to the target type {@link E}.
+     * If the mapper function is set to <code>null</code> the list acts as if it were empty
+     */
+    private final ObjectProperty<Function<? super F, ? extends E>> mapper;
 
+    /**
+     * A list of all mapped values
+     */
     private final List<E> mappedValues;
 
-    public MappedList(ObservableList<? extends F> source, Function<? super F, ? extends E> mapper) {
+    /**
+     * Constructor
+     *
+     * @param source The source list
+     * @param mapper The mapper function
+     */
+    public MappedList(ObservableList<? extends F> source, ObjectProperty<Function<? super F, ? extends E>> mapper) {
         super(source);
 
-        this.mappedValues = source.stream().map(mapper).collect(Collectors.toList());
         this.mapper = mapper;
+        this.mappedValues = new ArrayList<>();
 
+        // create a cache of all mapped source elements
+        Optional.ofNullable(getMapper())
+                .ifPresent(mapperFunction -> source.stream().map(mapperFunction).forEach(mappedValues::add));
+
+        // add a listener to detect changes of the mapper function
+        mapper.addListener((observable, oldMapper, newMapper) -> {
+            beginChange();
+            // the mapper function has been set to null
+            if (oldMapper != null && newMapper == null) {
+                mappedValues.clear();
+
+                nextRemove(0, mappedValues);
+            }
+
+            // the mapper function has been set from null to another value
+            if (oldMapper == null && newMapper != null) {
+                for (F element : getSource()) {
+                    mappedValues.add(newMapper.apply(element));
+                }
+
+                nextAdd(0, size());
+            }
+
+            // the mapper function has been changed (from not null to not null)
+            if (oldMapper != null && newMapper != null) {
+                mappedValues.clear();
+
+                for (int index = 0; index < size(); index++) {
+                    final F element = getSource().get(index);
+
+                    mappedValues.add(newMapper.apply(element));
+
+                    nextUpdate(index);
+                }
+            }
+            endChange();
+        });
+
+        // fire an initialisation event containing all mapped elements
         fireChange(new InitialisationChange<>(0, size(), this));
     }
 
+    /**
+     * Constructor
+     *
+     * @param source The source list
+     * @param mapper The mapper function
+     */
+    public MappedList(ObservableList<? extends F> source, Function<? super F, ? extends E> mapper) {
+        this(source, new SimpleObjectProperty<>(mapper));
+    }
+
+    /**
+     * Constructor
+     *
+     * @param source The source list
+     */
+    public MappedList(ObservableList<? extends F> source) {
+        this(source, new SimpleObjectProperty<>());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getSourceIndex(int index) {
         if (index >= size()) {
             throw new IndexOutOfBoundsException();
         }
+
         return index;
     }
 
@@ -46,30 +127,45 @@ public class MappedList<E, F> extends PhoenicisTransformationList<E, F> {
         if (index >= size()) {
             throw new IndexOutOfBoundsException();
         }
+
         return index;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public E get(int index) {
         if (index >= size()) {
             throw new IndexOutOfBoundsException();
         }
+
         return mappedValues.get(index);
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * If no mapper function is set, the size of this list is always <code>0</code>, otherwise it equals
+     * the size of the source list
+     */
     @Override
     public int size() {
-        return getSource().size();
+        return Optional.ofNullable(getMapper())
+                .map(mapper -> getSource().size()).orElse(0);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void permute(Change<? extends F> c) {
-        int from = c.getFrom();
-        int to = c.getTo();
+        final int from = c.getFrom();
+        final int to = c.getTo();
 
         if (to > from) {
-            List<E> clone = new ArrayList<>(mappedValues);
-            int[] perm = IntStream.range(0, size()).toArray();
+            final List<E> clone = new ArrayList<>(mappedValues);
+            final int[] perm = IntStream.range(0, size()).toArray();
 
             for (int i = from; i < to; ++i) {
                 perm[i] = c.getPermutation(i);
@@ -80,12 +176,17 @@ public class MappedList<E, F> extends PhoenicisTransformationList<E, F> {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void update(Change<? extends F> c) {
-        int from = c.getFrom();
-        int to = c.getTo();
+        final int from = c.getFrom();
+        final int to = c.getTo();
 
-        if (to > from) {
+        final Function<? super F, ? extends E> mapper = getMapper();
+
+        if (mapper != null) {
             for (int i = from; i < to; ++i) {
                 mappedValues.set(i, mapper.apply(getSource().get(i)));
 
@@ -94,18 +195,37 @@ public class MappedList<E, F> extends PhoenicisTransformationList<E, F> {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void addRemove(Change<? extends F> c) {
-        int from = c.getFrom();
+        final int from = c.getFrom();
 
-        for (int index = from + c.getRemovedSize() - 1; index >= from; index--) {
-            nextRemove(index, mappedValues.remove(index));
+        final Function<? super F, ? extends E> mapper = getMapper();
+
+        if (mapper != null) {
+            for (int index = from + c.getRemovedSize() - 1; index >= from; index--) {
+                nextRemove(index, mappedValues.remove(index));
+            }
+
+            for (int index = from; index < from + c.getAddedSize(); index++) {
+                mappedValues.add(index, mapper.apply(getSource().get(index)));
+
+                nextAdd(index, index + 1);
+            }
         }
+    }
 
-        for (int index = from; index < from + c.getAddedSize(); index++) {
-            mappedValues.add(index, mapper.apply(getSource().get(index)));
+    public Function<? super F, ? extends E> getMapper() {
+        return mapper.get();
+    }
 
-            nextAdd(index, index + 1);
-        }
+    public ObjectProperty<Function<? super F, ? extends E>> mapperProperty() {
+        return mapper;
+    }
+
+    public void setMapper(Function<? super F, ? extends E> mapper) {
+        this.mapper.set(mapper);
     }
 }
