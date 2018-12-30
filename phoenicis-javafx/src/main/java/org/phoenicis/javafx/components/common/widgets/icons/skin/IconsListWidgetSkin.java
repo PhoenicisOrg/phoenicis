e806509d466870fea2b5faaf32473f847eb27dc5
Quentin PARIS <git@quentin.paris>
2018-12-30 19:38:37 +0100

    Few exception fix

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/common/widgets/icons/skin/IconsListWidgetSkin.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/common/widgets/icons/skin/IconsListWidgetSkin.java
@@ -98,9 +98,11 @@ public class IconsListWidgetSkin<E> extends SkinBase<IconsListWidget<E>, IconsLi
         Optional.ofNullable(oldSelection).map(ListWidgetSelection::getSelection).ifPresent(selection -> {
             final int oldValueIndex = getControl().getElements().indexOf(selection);
 
-            IconsListElement<E> oldElement = mappedElements.get(oldValueIndex);
+            if(mappedElements.size() > oldValueIndex && oldValueIndex != -1) {
+                IconsListElement<E> oldElement = mappedElements.get(oldValueIndex);
 
-            oldElement.setSelected(false);
+                oldElement.setSelected(false);
+            }
         });
     }
 
