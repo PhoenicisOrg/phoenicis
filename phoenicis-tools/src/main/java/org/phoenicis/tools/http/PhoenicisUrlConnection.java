6b1e87c0379ba5daa147971c05e5851f776bc731
Quentin PARIS <git@quentin.paris>
2019-01-01 20:31:00 +0100

    Javadoc

--- phoenicis-tools/src/main/java/org/phoenicis/tools/http/PhoenicisUrlConnection.java
+++ phoenicis-tools/src/main/java/org/phoenicis/tools/http/PhoenicisUrlConnection.java
@@ -3,7 +3,6 @@ package org.phoenicis.tools.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Map;
 
@@ -18,8 +17,9 @@ class PhoenicisUrlConnection {
 
     /**
      * Creates the instance. Stores internally the {@link URL} and the {@link java.net.URLConnection} objects
+     *
      * @param urlConnection The URL connection
-     * @param url The URL
+     * @param url           The URL
      * @see #fromURL(URL) to build
      */
     private PhoenicisUrlConnection(HttpURLConnection urlConnection, URL url) {
@@ -30,6 +30,7 @@ class PhoenicisUrlConnection {
 
     /**
      * Constructor Equivalents to {@link URL#openConnection()}
+     *
      * @param url The URL
      * @return The {@link PhoenicisUrlConnection} instance
      * @throws IOException if any IO Error happens
@@ -43,22 +44,51 @@ class PhoenicisUrlConnection {
                 url);
     }
 
+    /**
+     * Initialize the connection
+     *
+     * @throws IOException if any IO error happens
+     */
     void connect() throws IOException {
-        this.followRedirectsAndGetReponseCode();
+        this.followRedirectsAndGetResponseCode();
     }
 
+    /**
+     * Get last modified header
+     *
+     * @return last modified
+     */
     long getLastModified() {
         return delegateUrlConnexion.getLastModified();
     }
 
+    /**
+     * Get connection input stream
+     *
+     * @return The connection input stream
+     * @throws IOException if any IO error happens
+     */
     InputStream getInputStream() throws IOException {
         return delegateUrlConnexion.getInputStream();
     }
 
+    /**
+     * Returns the value of the content-length header field as long
+     *
+     * @return the content-length of the resource
+     */
     long getContentLengthLong() {
         return delegateUrlConnexion.getContentLengthLong();
     }
 
+    /**
+     * Gets the filename of the remote resource. Follows HTTP redirects if this is required
+     * and reads content-disposition header if this is possible. Otherwise, it will parse
+     * the URL
+     *
+     * @return The filename of the remote resource
+     * @throws IOException if any IO error happens
+     */
     String fetchFileName() throws IOException {
         int responseCode = this.getReponseCode();
 
@@ -75,19 +105,29 @@ class PhoenicisUrlConnection {
         return fetchFilenameFromUrl();
     }
 
+    /**
+     * Gets HTTP response code
+     * @return http response code
+     * @throws IOException if any IO error happens
+     */
     private int getReponseCode() throws IOException {
-        return this.followRedirectsAndGetReponseCode();
+        return this.followRedirectsAndGetResponseCode();
     }
 
+    /**
+     * Determines the filename from the URL
+     * @return the filename
+     */
     private String fetchFilenameFromUrl() {
         return url.toExternalForm().substring(url.toExternalForm().lastIndexOf("/") + 1).split("\\?")[0];
     }
 
-    void setRequestMethod(String method) throws ProtocolException {
-        this.delegateUrlConnexion.setRequestMethod(method);
-    }
-
-    private int followRedirectsAndGetReponseCode() throws IOException {
+    /**
+     * Gets the response code after having follow HTTP redirects
+     * @return The Http response code
+     * @throws IOException if any IO error happens
+     */
+    private int followRedirectsAndGetResponseCode() throws IOException {
         if (responseCode == null) {
             responseCode = delegateUrlConnexion.getResponseCode();
         }
@@ -101,12 +141,16 @@ class PhoenicisUrlConnection {
             if (this.headers != null) {
                 setHeaders(headers);
             }
-            followRedirectsAndGetReponseCode();
+            followRedirectsAndGetResponseCode();
         }
 
         return responseCode;
     }
 
+    /**
+     * Sets HTTP headers
+     * @param headers Headers to be set
+     */
     void setHeaders(Map<String, String> headers) {
         this.headers = headers;
         headers.forEach(delegateUrlConnexion::setRequestProperty);
