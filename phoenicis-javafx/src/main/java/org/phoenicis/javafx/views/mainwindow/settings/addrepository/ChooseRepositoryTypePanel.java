e1585c705ae79fc7066dc5b6a852598fd4f159a6
plata <plata@mailbox.org>
2019-01-02 12:49:48 +0100

    Fix Codacy

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/settings/addrepository/ChooseRepositoryTypePanel.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/views/mainwindow/settings/addrepository/ChooseRepositoryTypePanel.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.util.StringConverter;
 
 import java.util.Arrays;
