42a5a6368ab65a5f4a9f4f2b8327c62a63c432c9
madoar <madoar@users.noreply.github.com>
2019-01-05 16:58:19 +0100

    Refactor the `LibraryPanel` with MVC in mind (#1712)
    - add a new LibraryDetailsPanel component
    - improve the LibraryView by replacing the callback setters through the concrete callback implementations
    - remove the old and now unused LibraryPanel class

---
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/library/skin/LibraryDetailsPanelSkin.java
@@ -0,0 +1,214 @@
+package org.phoenicis.javafx.components.library.skin;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import org.apache.commons.lang.StringUtils;
+import org.phoenicis.javafx.components.common.skin.DetailsPanelBaseSkin;
+import org.phoenicis.javafx.components.library.control.LibraryDetailsPanel;
+import org.phoenicis.javafx.controller.library.LibraryController;
+import org.phoenicis.javafx.views.common.ColumnConstraintsWithPercentage;
+import org.phoenicis.library.dto.ShortcutDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.phoenicis.configuration.localisation.Localisation.tr;
+
+/**
+ * {@link DetailsPanelBaseSkin} implementation class used inside the {@link LibraryDetailsPanel}
+ */
+public class LibraryDetailsPanelSkin extends DetailsPanelBaseSkin<LibraryDetailsPanel, LibraryDetailsPanelSkin> {
+    private final Logger LOGGER = LoggerFactory.getLogger(LibraryController.class);
+
+    /**
+     * The properties of the currently selected {@link ShortcutDTO}
+     */
+    private final ObservableMap<String, Object> shortcutProperties;
+
+    /**
+     * The description of the currently selected {@link ShortcutDTO}
+     */
+    private final StringProperty description;
+
+    /**
+     * Constructor
+     *
+     * @param control The control belonging to the skin
+     */
+    public LibraryDetailsPanelSkin(LibraryDetailsPanel control) {
+        super(control);
+
+        this.shortcutProperties = FXCollections.observableHashMap();
+        this.description = new SimpleStringProperty();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialise() {
+        super.initialise();
+
+        // ensure that the content of the details panel changes when the to be shown shortcut changes
+        getControl().shortcutProperty().addListener((Observable invalidation) -> updateShortcut());
+        // initialize the content of the details panel correctly
+        updateShortcut();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Node createContent() {
+        final Label descriptionLabel = new Label();
+        descriptionLabel.textProperty().bind(description);
+        descriptionLabel.setWrapText(true);
+
+        final GridPane propertiesGrid = createPropertiesGrid();
+
+        final Region spacer = new Region();
+        spacer.getStyleClass().add("detailsButtonSpacer");
+
+        final GridPane controlButtons = createControlButtons();
+
+        return new VBox(descriptionLabel, propertiesGrid, spacer, controlButtons);
+    }
+
+    /**
+     * Creates a new {@link GridPane} containing the properties of the selected shortcut
+     *
+     * @return a new {@link GridPane} containing the properties of the selected shortcut
+     */
+    private GridPane createPropertiesGrid() {
+        final GridPane propertiesGrid = new GridPane();
+        propertiesGrid.getStyleClass().add("grid");
+
+        ColumnConstraints titleColumn = new ColumnConstraintsWithPercentage(30);
+        ColumnConstraints valueColumn = new ColumnConstraintsWithPercentage(70);
+
+        propertiesGrid.getColumnConstraints().addAll(titleColumn, valueColumn);
+
+        // ensure that changes to the shortcutProperties map result in updates to the GridPane
+        shortcutProperties.addListener((Observable invalidation) -> updateProperties(propertiesGrid));
+        // initialize the properties grid correctly
+        updateProperties(propertiesGrid);
+
+        return propertiesGrid;
+    }
+
+    /**
+     * Creates a new {@link GridPane} containing the control buttons for the selected shortcut.
+     * These control buttons consist of:
+     * <ul>
+     * <li>The run button</li>
+     * <li>The stop button</li>
+     * <li>The uninstall button</li>
+     * </ul>
+     *
+     * @return A new {@link GridPane} containing the control buttons for the selected shortcut
+     */
+    private GridPane createControlButtons() {
+        final GridPane controlButtons = new GridPane();
+        controlButtons.getStyleClass().add("shortcut-control-button-group");
+
+        ColumnConstraints runColumn = new ColumnConstraintsWithPercentage(33.3);
+        ColumnConstraints stopColumn = new ColumnConstraintsWithPercentage(33.3);
+        ColumnConstraints uninstallColumn = new ColumnConstraintsWithPercentage(33.3);
+
+        controlButtons.getColumnConstraints().addAll(runColumn, stopColumn, uninstallColumn);
+
+        final Button runButton = new Button(tr("Run"));
+        runButton.getStyleClass().addAll("shortcutButton", "runButton");
+        runButton.setOnMouseClicked(event -> Optional.ofNullable(getControl().getOnShortcutRun())
+                .ifPresent(onShortcutRun -> onShortcutRun.accept(getControl().getShortcut())));
+        GridPane.setHalignment(runButton, HPos.CENTER);
+
+        final Button stopButton = new Button(tr("Close"));
+        stopButton.getStyleClass().addAll("shortcutButton", "stopButton");
+        stopButton.setOnMouseClicked(event -> Optional.ofNullable(getControl().getOnShortcutStop())
+                .ifPresent(onShortcutStop -> onShortcutStop.accept(getControl().getShortcut())));
+        GridPane.setHalignment(stopButton, HPos.CENTER);
+
+        final Button uninstallButton = new Button(tr("Uninstall"));
+        uninstallButton.getStyleClass().addAll("shortcutButton", "uninstallButton");
+        uninstallButton.setOnMouseClicked(event -> Optional.ofNullable(getControl().getOnShortcutUninstall())
+                .ifPresent(onShortcutUninstall -> onShortcutUninstall.accept(getControl().getShortcut())));
+        GridPane.setHalignment(uninstallButton, HPos.CENTER);
+
+        controlButtons.addRow(0, runButton, stopButton, uninstallButton);
+
+        return controlButtons;
+    }
+
+    /**
+     * Updates the shortcutProperties of the shortcut in the given {@link GridPane propertiesGrid}
+     *
+     * @param propertiesGrid The shortcutProperties grid
+     */
+    private void updateProperties(final GridPane propertiesGrid) {
+        propertiesGrid.getChildren().clear();
+
+        for (Map.Entry<String, Object> entry : shortcutProperties.entrySet()) {
+            final int row = propertiesGrid.getRowCount();
+
+            final Label keyLabel = new Label(tr(decamelize(entry.getKey())) + ":");
+            keyLabel.getStyleClass().add("captionTitle");
+            GridPane.setValignment(keyLabel, VPos.TOP);
+
+            final Label valueLabel = new Label(entry.getValue().toString());
+            valueLabel.setWrapText(true);
+
+            propertiesGrid.addRow(row, keyLabel, valueLabel);
+        }
+    }
+
+    /**
+     * Updates the shortcut of this {@link LibraryDetailsPanelSkin} instance
+     */
+    private void updateShortcut() {
+        final ShortcutDTO shortcut = getControl().getShortcut();
+
+        if (shortcut != null) {
+            title.setValue(shortcut.getInfo().getName());
+            description.setValue(shortcut.getInfo().getDescription());
+
+            try {
+                final Map<String, Object> shortcutProperties = getControl().getObjectMapper()
+                        .readValue(shortcut.getScript(), new TypeReference<Map<String, Object>>() {
+                            // nothing
+                        });
+
+                this.shortcutProperties.clear();
+                this.shortcutProperties.putAll(shortcutProperties);
+            } catch (IOException e) {
+                LOGGER.error("An error occurred during a shortcut update", e);
+            }
+        }
+    }
+
+    /**
+     * Decamelize the given string
+     *
+     * @param string The string to decamelize
+     * @return The decamelized string
+     */
+    private String decamelize(String string) {
+        return StringUtils.capitalize(StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(string), ' '));
+    }
+}
--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/controller/library/LibraryController.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/controller/library/LibraryController.java
@@ -19,108 +19,33 @@
 package org.phoenicis.javafx.controller.library;
 
 import javafx.application.Platform;
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import org.phoenicis.javafx.controller.library.console.ConsoleController;
-import org.phoenicis.javafx.dialogs.ConfirmDialog;
-import org.phoenicis.javafx.dialogs.ErrorDialog;
 import org.phoenicis.javafx.views.mainwindow.library.LibraryView;
 import org.phoenicis.library.LibraryManager;
-import org.phoenicis.library.ShortcutManager;
-import org.phoenicis.library.ShortcutRunner;
 import org.phoenicis.library.dto.ShortcutCategoryDTO;
-import org.phoenicis.library.dto.ShortcutCreationDTO;
-import org.phoenicis.library.dto.ShortcutDTO;
 import org.phoenicis.repository.RepositoryManager;
 import org.phoenicis.repository.dto.RepositoryDTO;
-import org.phoenicis.scripts.interpreter.InteractiveScriptSession;
-import org.phoenicis.scripts.interpreter.ScriptInterpreter;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Collections;
 import java.util.List;
 
-import static org.phoenicis.configuration.localisation.Localisation.tr;
-
 public class LibraryController {
-    private final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(LibraryController.class);
-    @Value("${application.user.containers}")
-    private String containersPath;
-
     private final LibraryView libraryView;
     private final LibraryManager libraryManager;
-    private final ShortcutRunner shortcutRunner;
-    private final ShortcutManager shortcutManager;
-    private final ScriptInterpreter scriptInterpreter;
     private final RepositoryManager repositoryManager;
 
     private boolean firstViewSelection = true;
 
     public LibraryController(LibraryView libraryView, ConsoleController consoleController,
-            LibraryManager libraryManager, ShortcutRunner shortcutRunner, ShortcutManager shortcutManager,
-            ScriptInterpreter scriptInterpreter, RepositoryManager repositoryManager) {
+            LibraryManager libraryManager, RepositoryManager repositoryManager) {
+        super();
+
         this.libraryView = libraryView;
         this.libraryManager = libraryManager;
-        this.shortcutRunner = shortcutRunner;
-        this.shortcutManager = shortcutManager;
-        this.scriptInterpreter = scriptInterpreter;
         this.repositoryManager = repositoryManager;
 
-        libraryManager.setOnUpdate(this::updateLibrary);
+        this.libraryManager.setOnUpdate(this::updateLibrary);
 
-        this.libraryView.setOnShortcutCreate(this::createShortcut);
-        this.libraryView.setOnShortcutRun(this::runShortcut);
-        this.libraryView.setOnShortcutDoubleClicked(this::runShortcut);
-        this.libraryView.setOnShortcutStop(
-                shortcutDTO -> shortcutRunner.stop(shortcutDTO, e -> {
-                    final ErrorDialog errorDialog = ErrorDialog.builder()
-                            .withMessage(tr("Error"))
-                            .withException(e)
-                            .withOwner(this.libraryView.getContent().getScene().getWindow())
-                            .build();
-
-                    errorDialog.showAndWait();
-                }));
-
-        this.libraryView.setOnShortcutUninstall(shortcutDTO -> {
-            final String shortcutName = shortcutDTO.getInfo().getName();
-
-            final ConfirmDialog confirmMessage = ConfirmDialog.builder()
-                    .withTitle(tr("Uninstall {0}", shortcutName))
-                    .withMessage(tr("Are you sure you want to uninstall {0}?", shortcutName))
-                    .withOwner(libraryView.getContent().getScene().getWindow())
-                    .withResizable(true)
-                    .withYesCallback(() -> shortcutManager.uninstallFromShortcut(shortcutDTO, e -> {
-                        final ErrorDialog errorDialog = ErrorDialog.builder()
-                                .withMessage(tr("Error while uninstalling {0}", shortcutName))
-                                .withException(e)
-                                .withOwner(this.libraryView.getContent().getScene().getWindow())
-                                .build();
-
-                        errorDialog.showAndWait();
-                    }))
-                    .build();
-
-            confirmMessage.showAndCallback();
-        });
-
-        this.libraryView.setOnShortcutChanged(shortcutDTO -> this.shortcutManager.updateShortcut(shortcutDTO));
-
-        this.libraryView.setOnOpenConsole(() -> {
-            libraryView.createNewTab(consoleController.createConsole());
-        });
-
-        this.libraryView.setOnScriptRun(file -> {
-            scriptInterpreter.runScript(file, e -> Platform.runLater(() -> {
-                final ErrorDialog errorDialog = ErrorDialog.builder()
-                        .withMessage(tr("Error while running script"))
-                        .withException(e)
-                        .withOwner(this.libraryView.getContent().getScene().getWindow())
-                        .build();
-
-                errorDialog.showAndWait();
-            }));
-        });
+        this.libraryView.setOnOpenConsole(() -> libraryView.createNewTab(consoleController.createConsole()));
 
         this.libraryView.setOnSelectionChanged(event -> {
             if (this.libraryView.isSelected() && this.firstViewSelection) {
@@ -130,75 +55,6 @@ public class LibraryController {
                 this.firstViewSelection = false;
             }
         });
-    }
-
-    /**
-     * creates a new shortcut
-     *
-     * @param shortcutCreationDTO DTO describing the new shortcut
-     */
-    private void createShortcut(ShortcutCreationDTO shortcutCreationDTO) {
-        // get container
-        // TODO: smarter way using container manager
-        final String executablePath = shortcutCreationDTO.getExecutable().getAbsolutePath();
-        final String pathInContainers = executablePath.replace(containersPath, "");
-        final String[] split = pathInContainers.split("/");
-        final String engineContainer = split[0];
-        final String engine = (Character.toUpperCase(engineContainer.charAt(0)) + engineContainer.substring(1))
-                .replace("prefix", "");
-        // TODO: better way to get engine ID
-        final String engineId = engine.toLowerCase();
-        final String container = split[1];
-
-        final InteractiveScriptSession interactiveScriptSession = scriptInterpreter.createInteractiveSession();
-
-        interactiveScriptSession.eval(
-                "include([\"engines\", \"" + engineId + "\", \"shortcuts\", \"" + engineId + "\"]);",
-                ignored -> interactiveScriptSession.eval("new " + engine + "Shortcut()", output -> {
-                    final ScriptObjectMirror shortcutObject = (ScriptObjectMirror) output;
-                    shortcutObject.callMember("name", shortcutCreationDTO.getName());
-                    shortcutObject.callMember("category", shortcutCreationDTO.getCategory());
-                    shortcutObject.callMember("description", shortcutCreationDTO.getDescription());
-                    shortcutObject.callMember("miniature", shortcutCreationDTO.getMiniature());
-                    shortcutObject.callMember("search", shortcutCreationDTO.getExecutable().getName());
-                    shortcutObject.callMember("prefix", container);
-                    shortcutObject.callMember("create");
-                }, e -> this.showErrorMessage(e, tr("Error while creating shortcut"))),
-                e -> this.showErrorMessage(e, tr("Error while creating shortcut")));
-    }
-
-    private void runShortcut(ShortcutDTO shortcutDTO) {
-        shortcutRunner.run(shortcutDTO, Collections.emptyList(), e -> Platform.runLater(() -> {
-            final ErrorDialog errorDialog = ErrorDialog.builder()
-                    .withMessage(tr("Error"))
-                    .withException(e)
-                    .withOwner(this.libraryView.getContent().getScene().getWindow())
-                    .build();
-
-            errorDialog.showAndWait();
-        }));
-    }
-
-    /**
-     * shows an error message
-     *
-     * @param e exception that caused the error
-     * @param message error message
-     */
-    private void showErrorMessage(Exception e, String message) {
-        Platform.runLater(() -> {
-            final ErrorDialog errorDialog = ErrorDialog.builder()
-                    .withMessage(message)
-                    .withException(e)
-                    .withOwner(this.libraryView.getContent().getScene().getWindow())
-                    .build();
-
-            errorDialog.showAndWait();
-        });
-    }
-
-    public void setOnTabOpened(Runnable onTabOpened) {
-        this.libraryView.setOnTabOpened(onTabOpened);
     }
 
     /**
