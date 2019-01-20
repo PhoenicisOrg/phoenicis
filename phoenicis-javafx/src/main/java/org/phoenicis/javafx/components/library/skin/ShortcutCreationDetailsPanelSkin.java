00df5990d095e2884fb5e2f217a5c08c5573ba2d
Marc Arndt <marc.arndt@masimi.de>
2019-01-20 12:55:27 +0100

    - make Codacy happy

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/library/skin/ShortcutCreationDetailsPanelSkin.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/library/skin/ShortcutCreationDetailsPanelSkin.java
@@ -43,104 +43,25 @@ public class ShortcutCreationDetailsPanelSkin
         gridPane.getColumnConstraints().addAll(labelConstraints, valueConstraints);
 
         // name
-        final Label nameLabel = new Label(tr("Name:"));
-        nameLabel.getStyleClass().add("captionTitle");
-
-        GridPane.setValignment(nameLabel, VPos.TOP);
-
-        final TextField name = new TextField();
-
-        gridPane.addRow(0, nameLabel, name);
+        final TextField name = addName(gridPane);
 
         final Tooltip nameErrorTooltip = new Tooltip(tr("Please specify a name!"));
 
         // category
-        final Label categoryLabel = new Label(tr("Category:"));
-        categoryLabel.getStyleClass().add("captionTitle");
-
-        GridPane.setValignment(categoryLabel, VPos.TOP);
-
-        final TextField category = new TextField();
-
-        gridPane.addRow(1, categoryLabel, category);
+        final TextField category = addCategory(gridPane);
 
         final Tooltip categoryErrorTooltip = new Tooltip(tr("Please specify a category!"));
 
         // description
-        final Label descriptionLabel = new Label(tr("Description:"));
-        descriptionLabel.getStyleClass().add("captionTitle");
-
-        GridPane.setValignment(descriptionLabel, VPos.TOP);
-
-        final TextArea description = new TextArea();
-
-        gridPane.addRow(2, descriptionLabel, description);
+        final TextArea description = addDescription(gridPane);
 
         // miniature
-        final Label miniatureLabel = new Label(tr("Miniature:"));
-        miniatureLabel.getStyleClass().add("captionTitle");
-
-        GridPane.setValignment(miniatureLabel, VPos.TOP);
-
-        final TextField miniature = new TextField();
-
-        final Button openMiniatureBrowser = new Button(tr("Browse..."));
-        openMiniatureBrowser.setOnAction(event -> {
-            FileChooser chooser = new FileChooser();
-
-            // open in containers directory if it exists
-            File containersDir = new File(getControl().getContainersPath());
-            if (containersDir.canRead()) {
-                chooser.setInitialDirectory(containersDir);
-            }
-
-            chooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter(tr("Images"), "*.miniature, *.png"));
-
-            File newMiniature = chooser.showOpenDialog(getControl().getScene().getWindow());
-            if (newMiniature != null) {
-                miniature.setText(newMiniature.toString());
-            }
-        });
-
-        final HBox miniatureHbox = new HBox(miniature, openMiniatureBrowser);
-        miniatureHbox.getStyleClass().add("file-selection-container");
-
-        HBox.setHgrow(miniature, Priority.ALWAYS);
-
-        gridPane.addRow(3, miniatureLabel, miniatureHbox);
+        final TextField miniature = addMiniature(gridPane);
 
         final Tooltip miniatureErrorTooltip = new Tooltip(tr("Please specify a valid miniature!"));
 
         // executable
-        final Label executableLabel = new Label(tr("Executable:"));
-        executableLabel.getStyleClass().add("captionTitle");
-
-        GridPane.setValignment(executableLabel, VPos.TOP);
-
-        final TextField executable = new TextField();
-
-        final Button openExecutableBrowser = new Button(tr("Browse..."));
-        openExecutableBrowser.setOnAction(event -> {
-            FileChooser chooser = new FileChooser();
-
-            // open in containers directory if it exists
-            File containersDir = new File(getControl().getContainersPath());
-            if (containersDir.canRead()) {
-                chooser.setInitialDirectory(containersDir);
-            }
-
-            File newExecutable = chooser.showOpenDialog(getControl().getScene().getWindow());
-            if (newExecutable != null) {
-                executable.setText(newExecutable.toString());
-            }
-        });
-
-        final HBox executableHbox = new HBox(executable, openExecutableBrowser);
-        miniatureHbox.getStyleClass().add("file-selection-container");
-
-        HBox.setHgrow(executable, Priority.ALWAYS);
-
-        gridPane.addRow(4, executableLabel, executableHbox);
+        final TextField executable = addExecutable(gridPane);
 
         final Tooltip executableErrorTooltip = new Tooltip(tr("Please specify a valid executable!"));
 
@@ -198,5 +119,124 @@ public class ShortcutCreationDetailsPanelSkin
         container.getStyleClass().addAll("library-details-panel-content", "create-shortcut-panel-content");
 
         return container;
+    }
+
+    private TextField addName(final GridPane gridPane) {
+        final int row = gridPane.getRowCount();
+
+        final Label nameLabel = new Label(tr("Name:"));
+        nameLabel.getStyleClass().add("captionTitle");
+
+        GridPane.setValignment(nameLabel, VPos.TOP);
+
+        final TextField name = new TextField();
+
+        gridPane.addRow(row, nameLabel, name);
+
+        return name;
+    }
+
+    private TextField addCategory(final GridPane gridPane) {
+        final int row = gridPane.getRowCount();
+
+        final Label categoryLabel = new Label(tr("Category:"));
+        categoryLabel.getStyleClass().add("captionTitle");
+
+        GridPane.setValignment(categoryLabel, VPos.TOP);
+
+        final TextField category = new TextField();
+
+        gridPane.addRow(row, categoryLabel, category);
+
+        return category;
+    }
+
+    private TextArea addDescription(final GridPane gridPane) {
+        final int row = gridPane.getRowCount();
+
+        final Label descriptionLabel = new Label(tr("Description:"));
+        descriptionLabel.getStyleClass().add("captionTitle");
+
+        GridPane.setValignment(descriptionLabel, VPos.TOP);
+
+        final TextArea description = new TextArea();
+
+        gridPane.addRow(row, descriptionLabel, description);
+
+        return description;
+    }
+
+    private TextField addMiniature(final GridPane gridPane) {
+        final int row = gridPane.getRowCount();
+
+        final Label miniatureLabel = new Label(tr("Miniature:"));
+        miniatureLabel.getStyleClass().add("captionTitle");
+
+        GridPane.setValignment(miniatureLabel, VPos.TOP);
+
+        final TextField miniature = new TextField();
+
+        final Button openMiniatureBrowser = new Button(tr("Browse..."));
+        openMiniatureBrowser.setOnAction(event -> {
+            FileChooser chooser = new FileChooser();
+
+            // open in containers directory if it exists
+            File containersDir = new File(getControl().getContainersPath());
+            if (containersDir.canRead()) {
+                chooser.setInitialDirectory(containersDir);
+            }
+
+            chooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter(tr("Images"), "*.miniature, *.png"));
+
+            File newMiniature = chooser.showOpenDialog(getControl().getScene().getWindow());
+            if (newMiniature != null) {
+                miniature.setText(newMiniature.toString());
+            }
+        });
+
+        final HBox miniatureHbox = new HBox(miniature, openMiniatureBrowser);
+        miniatureHbox.getStyleClass().add("file-selection-container");
+
+        HBox.setHgrow(miniature, Priority.ALWAYS);
+
+        gridPane.addRow(row, miniatureLabel, miniatureHbox);
+
+        return miniature;
+    }
+
+    private TextField addExecutable(final GridPane gridPane) {
+        final int row = gridPane.getRowCount();
+
+        final Label executableLabel = new Label(tr("Executable:"));
+        executableLabel.getStyleClass().add("captionTitle");
+
+        GridPane.setValignment(executableLabel, VPos.TOP);
+
+        final TextField executable = new TextField();
+
+        final Button openExecutableBrowser = new Button(tr("Browse..."));
+        openExecutableBrowser.setOnAction(event -> {
+            FileChooser chooser = new FileChooser();
+
+            // open in containers directory if it exists
+            File containersDir = new File(getControl().getContainersPath());
+            if (containersDir.canRead()) {
+                chooser.setInitialDirectory(containersDir);
+            }
+
+            File newExecutable = chooser.showOpenDialog(getControl().getScene().getWindow());
+            if (newExecutable != null) {
+                executable.setText(newExecutable.toString());
+            }
+        });
+
+        final HBox executableHbox = new HBox(executable, openExecutableBrowser);
+        executableHbox.getStyleClass().add("file-selection-container");
+
+        HBox.setHgrow(executable, Priority.ALWAYS);
+
+        gridPane.addRow(row, executableLabel, executableHbox);
+
+        return executable;
     }
 }
