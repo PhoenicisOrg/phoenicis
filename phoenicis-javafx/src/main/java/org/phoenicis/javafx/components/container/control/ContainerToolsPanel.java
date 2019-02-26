6198670161f0be0235b5266a770d7270914d8f7b
madoar <madoar@users.noreply.github.com>
2019-02-26 17:37:19 +0100

    Refactor the ContainerToolsTab class with MVC in mind (#1862)
    - refactor the ContainerToolsTab class with MVC in mind

---
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/container/control/ContainerToolsPanel.java
@@ -0,0 +1,97 @@
+package org.phoenicis.javafx.components.container.control;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import org.phoenicis.containers.ContainerEngineController;
+import org.phoenicis.containers.dto.ContainerDTO;
+import org.phoenicis.javafx.components.common.control.ControlBase;
+import org.phoenicis.javafx.components.container.skin.ContainerToolsPanelSkin;
+
+/**
+ * A component used to execute tools inside an existing container
+ */
+public class ContainerToolsPanel extends ControlBase<ContainerToolsPanel, ContainerToolsPanelSkin> {
+    /**
+     * The container
+     */
+    private final ObjectProperty<ContainerDTO> container;
+
+    /**
+     * The container engine controller used to execute a tool inside a container
+     */
+    private final ObjectProperty<ContainerEngineController> containerEngineController;
+
+    /**
+     * A boolean signifying whether all tool buttons should be locked
+     */
+    private final BooleanProperty lockTools;
+
+    /**
+     * Constructor
+     *
+     * @param container The container
+     * @param containerEngineController The container engine controller used to execute a tool inside a container
+     * @param lockTools A boolean signifying whether all tool buttons should be locked
+     */
+    public ContainerToolsPanel(ObjectProperty<ContainerDTO> container,
+            ObjectProperty<ContainerEngineController> containerEngineController, BooleanProperty lockTools) {
+        super();
+
+        this.container = container;
+        this.containerEngineController = containerEngineController;
+        this.lockTools = lockTools;
+    }
+
+    /**
+     * Constructor
+     */
+    public ContainerToolsPanel() {
+        this(new SimpleObjectProperty<>(), new SimpleObjectProperty<>(), new SimpleBooleanProperty());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ContainerToolsPanelSkin createSkin() {
+        return new ContainerToolsPanelSkin(this);
+    }
+
+    public ContainerDTO getContainer() {
+        return this.container.get();
+    }
+
+    public ObjectProperty<ContainerDTO> containerProperty() {
+        return this.container;
+    }
+
+    public void setContainer(ContainerDTO container) {
+        this.container.set(container);
+    }
+
+    public ContainerEngineController getContainerEngineController() {
+        return this.containerEngineController.get();
+    }
+
+    public ObjectProperty<ContainerEngineController> containerEngineControllerProperty() {
+        return this.containerEngineController;
+    }
+
+    public void setContainerEngineController(ContainerEngineController containerEngineController) {
+        this.containerEngineController.set(containerEngineController);
+    }
+
+    public boolean isLockTools() {
+        return this.lockTools.get();
+    }
+
+    public BooleanProperty lockToolsProperty() {
+        return this.lockTools;
+    }
+
+    public void setLockTools(boolean lockTools) {
+        this.lockTools.set(lockTools);
+    }
+}
