7bead68da0c366767c756b6223f3b111cf77ddce
plata <plata@mailbox.org>
2019-03-06 13:38:49 +0100

    Remove unused import

--- phoenicis-engines/src/main/java/org/phoenicis/engines/EngineSettingsManager.java
+++ phoenicis-engines/src/main/java/org/phoenicis/engines/EngineSettingsManager.java
@@ -24,7 +24,6 @@ import org.phoenicis.scripts.interpreter.InteractiveScriptSession;
 import org.phoenicis.scripts.interpreter.ScriptInterpreter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
