dd68ac5062ab91bfb06aae075ff139be05f6f8e6
plata <plata@users.noreply.github.com>
2019-06-22 22:00:06 +0200

    Log exception

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/JavaFXApplication.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/JavaFXApplication.java
@@ -26,8 +26,11 @@ import org.phoenicis.javafx.controller.MainController;
 import org.phoenicis.multithreading.ControlledThreadPoolExecutorServiceCloser;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JavaFXApplication extends Application {
+    private final static Logger LOGGER = LoggerFactory.getLogger(JavaFXApplication.class);
 
     public static void main(String[] args) {
         try {
@@ -52,6 +55,7 @@ public class JavaFXApplication extends Application {
                 applicationContext.getBean(ControlledThreadPoolExecutorServiceCloser.class).setCloseImmediately(true);
                 applicationContext.close();
             } catch (Exception e) {
+                LOGGER.warn("Exception while closing the application.", e);
             }
         });
     }
