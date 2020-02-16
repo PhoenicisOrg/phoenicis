3d87f075d5c930b0bd05169426dff9ea351641fe
plata <plata@mailbox.org>
2020-02-16 20:28:56 +0100

    Add TODO

--- phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/library/control/LibraryFeaturePanel.java
+++ phoenicis-javafx/src/main/java/org/phoenicis/javafx/components/library/control/LibraryFeaturePanel.java
@@ -231,15 +231,18 @@ public class LibraryFeaturePanel extends FeaturePanel<LibraryFeaturePanel, Libra
                 .withMessage(tr("Are you sure you want to uninstall {0}?", shortcutName))
                 .withOwner(getScene().getWindow())
                 .withResizable(true)
-                .withYesCallback(() -> getShortcutManager().uninstallFromShortcut(shortcut, e -> {
-                    final ErrorDialog errorDialog = ErrorDialog.builder()
-                            .withMessage(tr("Error while uninstalling {0}", shortcutName))
-                            .withException(e)
-                            .withOwner(getScene().getWindow())
-                            .build();
+                .withYesCallback(() -> {
+                    getShortcutManager().uninstallFromShortcut(shortcut, e -> {
+                        final ErrorDialog errorDialog = ErrorDialog.builder()
+                                .withMessage(tr("Error while uninstalling {0}", shortcutName))
+                                .withException(e)
+                                .withOwner(getScene().getWindow())
+                                .build();
 
-                    errorDialog.showAndWait();
-                }))
+                        errorDialog.showAndWait();
+                    });
+                    // TODO: how can the feature panel close itself?
+                })
                 .build();
 
         confirmMessage.showAndCallback();
