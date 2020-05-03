872b79e55d3751aad195eac65c1938957cd1dc21
plata <plata@users.noreply.github.com>
2020-05-03 23:05:50 +0200

    CLI: fix app installation (#2204)
    * Optimize imports
    
    * Fix unit test
    
    * Add missing new line at EOF

--- phoenicis-cli/src/main/java/org/phoenicis/cli/CLIController.java
+++ phoenicis-cli/src/main/java/org/phoenicis/cli/CLIController.java
@@ -22,10 +22,12 @@ import com.github.jankroken.commandline.annotations.AllAvailableArguments;
 import com.github.jankroken.commandline.annotations.LongSwitch;
 import com.github.jankroken.commandline.annotations.Option;
 import com.github.jankroken.commandline.annotations.ShortSwitch;
+import org.graalvm.polyglot.Value;
 import org.phoenicis.library.ShortcutRunner;
 import org.phoenicis.multithreading.ControlledThreadPoolExecutorServiceCloser;
 import org.phoenicis.repository.RepositoryManager;
 import org.phoenicis.repository.dto.ScriptDTO;
+import org.phoenicis.scripts.Installer;
 import org.phoenicis.scripts.interpreter.ScriptInterpreter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -88,19 +90,37 @@ public class CLIController implements AutoCloseable {
     @AllAvailableArguments
     public void installApp(List<String> arguments) {
         final String typeName = arguments.get(0);
+        final String typeId = typeName;
         final String categoryName = arguments.get(1);
+        final String categoryId = typeId + "." + categoryName;
         final String appName = arguments.get(2);
+        final String appId = categoryId + "." + appName;
         final String scriptName = arguments.get(3);
+        final String scriptId = appId + "." + scriptName;
 
         final ScriptDTO scriptDTO = repositoryManager
-                .getScript(Arrays.asList(typeName, categoryName, appName, scriptName));
+                .getScript(Arrays.asList(typeId, categoryId, appId, scriptId));
 
         if (scriptDTO == null) {
             LOGGER.error("Requested app does not exist: " + arguments);
             return;
         }
 
-        scriptInterpreter.runScript(scriptDTO.getScript(), Throwable::printStackTrace);
+        final StringBuilder executeBuilder = new StringBuilder();
+        executeBuilder.append(String.format("TYPE_ID=\"%s\";\n", scriptDTO.getTypeId()));
+        executeBuilder.append(String.format("CATEGORY_ID=\"%s\";\n", scriptDTO.getCategoryId()));
+        executeBuilder.append(String.format("APPLICATION_ID=\"%s\";\n", scriptDTO.getApplicationId()));
+        executeBuilder.append(String.format("SCRIPT_ID=\"%s\";\n", scriptDTO.getId()));
+
+        executeBuilder.append(scriptDTO.getScript());
+        executeBuilder.append("\n");
+
+        scriptInterpreter.createInteractiveSession()
+                .eval(executeBuilder.toString(), result -> {
+                    Value installer = (Value) result;
+
+                    installer.as(Installer.class).go();
+                }, Throwable::printStackTrace);
     }
 
     @Override
