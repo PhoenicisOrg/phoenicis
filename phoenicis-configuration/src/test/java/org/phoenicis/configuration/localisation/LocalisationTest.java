b46ecd6e4f0c5dc2026d8e35d157ddb43921ccc4
plata <plata@mailbox.org>
2021-10-11 17:28:44 +0200

    Fix mockito imports

--- phoenicis-configuration/src/test/java/org/phoenicis/configuration/localisation/LocalisationTest.java
+++ phoenicis-configuration/src/test/java/org/phoenicis/configuration/localisation/LocalisationTest.java
@@ -12,8 +12,8 @@ import org.xnap.commons.i18n.I18nFactory;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.phoenicis.configuration.localisation.Localisation.tr;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
